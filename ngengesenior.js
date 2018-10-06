c96b3f2485b3f4e3859ab492a2f417cb8cf44bed
Ngenge Senior <seniorngenge@gmail.com>
2018-10-06 05:50:44 +0100

    Added ngengesenor.js

---
+++ ngengesenior.js
@@ -0,0 +1,8 @@
+export default {
+    name: 'Ngenge Senior',
+    github: 'ngengesenior',
+    email: 'seniorngenge@gmail.com',
+    twitter: '@ngenge_senior',
+    facebook: 'ngengesenior',
+    linkedin: 'in/ngengesenior'
+  }
