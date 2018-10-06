118f912cc7abcc07bcfe1b8fc8c15748b98fbf46
Irfan Maulana <mazipanneh@gmail.com>
2018-10-06 10:12:44 +0700

    Create mazipan.js

---
+++ mazipan.js
@@ -0,0 +1,8 @@
+export default {
+  name: 'Irfan Maualan',
+  github: 'mazipan',
+  email: 'mazipanneh@gmail.com',
+  twitter: '@Maz_Ipan',
+  facebook: 'mazipanneh',
+  linkedin: 'in/irfanmaulanamazipan'
+}
