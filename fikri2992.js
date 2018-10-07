44145c7294755974e5c9e7cddb2c648f2f2cb85e
Andre Christoga <andrexchristoga@gmail.com>
2018-10-07 20:26:11 +0700

    Moved files to peoples/ dir

--- fikri2992.js
+++
@@ -1,8 +0,0 @@
-export default {
-  name: 'kang bubur naik haji' 
-  github: 'https://github.com/fikri2992',
-  email: 'crylikebaby2992@gmail.com',
-  twitter: '@fikri2992',
-  facebook: 'fikrin2',
-  linkedin: 'in/fikri nugraha'
-}
--- studentdawid.js
+++
@@ -1,5 +0,0 @@
-export default {
-    name: 'Dawid Kozak',
-    github: 'StudentDawid',
-    email: 'd.kozak92@gmail.com'
-  }
