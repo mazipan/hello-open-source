22062692c1e4ab0d8ff0ba6e7b6d8963705cac4b
Danang Eko Alfianto <danang.ekal@vizitrip.com>
2018-10-08 10:11:06 +0700

    Add People danangekal.js

---
+++ peoples/danangekal.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Danang Eko Alfianto',
+    github: 'danangekal',
+    email: 'danangekal@gmail.com',
+    twitter: '@danang_ekal',
+    facebook: 'Danang.Eko.Alfianto',
+    linkedin: 'in/danang-eko-alfianto-98bb2967'
+}
