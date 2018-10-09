52d15d1b0b2acf6c9a1469174a7728254716700d
Joko Susilo <susilo.j8@gmail.com>
2018-10-09 19:02:18 +0700

    add people jokosu10.js

---
+++ peoples/jokosu10.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Joko Susilo',
+    github: 'jokosu10',
+    email: 'susilo.j8@gmail.com',
+    twitter: '@jokosu_10',
+    facebook: 'JokoKikoSusilo',
+    linkedin: 'in/jokosu10'
+}
