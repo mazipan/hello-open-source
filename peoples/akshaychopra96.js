179b1d9905e167781bab9ecae60043726a04fba9
Akshay <akshaychopra96@gmail.com>
2018-10-14 15:08:49 +0530

    my contribution

---
+++ peoples/akshaychopra96.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Akshay Chopra',
+  github: 'akshaychopra96',
+  email: 'akshaychopra96@yahoo.co.in',
+  twitter: '',
+  facebook: '',
+  linkedin: 'in/akshaychopra1996'
+}
