7b986cf41785db513ac4db29436a9dae65b2e4c6
Avnish Kumar Kashyap <37414321+kashyap02@users.noreply.github.com>
2018-10-16 04:06:10 +0530

    User Details (#26)
    * Added User Detail
    
    * deleted
    
    * Added User Detail

---
+++ peoples/github_kashyap02.js
@@ -0,0 +1,8 @@
+modules.export = {
+    name: 'Ritesh Kashyap',
+    github: 'https://github.com/kashyap02',
+    email: 'kashyap02@gmail.com',
+    twitter: 'kashyap02',
+    facebook: 'avnish.9.kashyap',
+    linkedin: 'in/kashyap'
+}
