8de9d79cff35f4e842139b032f804a3c264f8619
Avnish <avnish21kashyap@gmail.com>
2018-10-15 22:49:37 +0530

    deleted

--- peoples/github_kashyap02.js
+++
@@ -1,8 +0,0 @@
-modules.export = {
-    name: 'Ritesh Kashyap',
-    github: 'https://github.com/kashyap02',
-    email: 'kashyap02@gmail.com',
-    twitter: 'kashyap02',
-    facebook: 'avnish.9.kashyap',
-    linkedin: 'in/kashyap'
-}
