0d54d37f2e816d756e21a84d007fd03d1bc0bef0
Sudhanshu Rawat <rawatsudhanshu15@gmail.com>
2018-10-24 23:56:13 +0530

    This is myfile

---
+++ peoples/sudhanshu.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Sudhanshu Rawat',
+  github: 'phenom57',
+  email: 'rawatsudhanshu@gmail.com',
+  twitter: 'rawatat007',
+  facebook: 'suahanshurawat.09',
+  linkedin: 'king'
+}
