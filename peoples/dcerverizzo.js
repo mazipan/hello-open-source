1db4ca8307f3db1c6d2e615d402e33f4ddd82b4f
Daniel Cerverizzo <=>
2018-10-25 00:20:39 -0300

    dcerverizzo

---
+++ peoples/dcerverizzo.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'Daniel Cerverizzo',
+    github: 'dcerverizzo',
+    email: 'danielcerverizzo@gmail.com',
+    twitter: '@eitapainho',
+    facebook: '',
+    linkedin: 'in/daniel-cerverizzo'
+  }
+  
