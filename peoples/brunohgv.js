99937cfdae8bd96e8abb71fc87320d18673ab6b6
Bruno Henrique Gusmão Vasconcelos <brunohgv2@gmail.com>
2018-10-25 15:11:40 -0300

    Now i have my name (#34)

---
+++ peoples/brunohgv.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Bruno Vasconcelos',
+  github: 'brunohgv',
+  email: 'brunohgv2@gmail.com',
+  twitter: '',
+  facebook: '',
+  linkedin: 'in/bruno-vasconcelos-7a1a8113b'
+}
