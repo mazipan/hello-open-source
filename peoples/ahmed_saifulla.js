ad83a5950375ef7485289cc615b24bdb67d8c718
Ahmed Saifulla N S <30487414+ahmed-saifulla@users.noreply.github.com>
2018-10-28 04:44:10 +0530

    Myname (#36)
    * Added myname.js module
    
    * Revert "Added myname.js module"
    
    This reverts commit e22abe10a5f52071b87514eb4590d14345d966f6.
    
    * added myname.js

---
+++ peoples/ahmed_saifulla.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Ahmed Saifulla',
+  github: 'ahmed-saifulla',
+  email: 'ahsaifulla.com',
+  twitter: '@ahsaifulla',
+  facebook: 'saifuksd',
+  linkedin: 'in/ahsaifulla'
+}
