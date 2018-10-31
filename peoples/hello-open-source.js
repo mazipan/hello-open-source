d78bb7e1f54e75722ee8a30dd139dc06375b8528
Kanthraj S <kanthraj.s@icloud.com>
2018-11-01 05:15:33 +0530

    new data added (#39)

---
+++ peoples/hello-open-source.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'test-username',
+  github: 'test-github',
+  email: 'test@gmail.com',
+  twitter: '@test_twitter',
+  facebook: 'test_facebook',
+  linkedin: 'in/test_linkedin'
+}
