a5f370204901ed7978bd1a0582977f8d3f4d2703
Brenno Barboza <brennol.c.b@hotmail.com>
2018-10-31 21:02:54 -0300

    Hello Open Source (#35) (#38)

---
+++ peoples/DwarfThief.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Brenno Barboza',
+  github: 'DwarfThief',
+  email: 'brennol.c.b@hotmail.com',
+  twitter: '@dwarfthief',
+  facebook: 'dwarfthief',
+  linkedin: 'in/dwarfthief'
+}
