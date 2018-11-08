5b939176b523c0024597ea376cd5f2839bb6ef32
MRobinson <mrobinson928@hotmail.com>
2018-11-08 12:02:34 -0500

    I have added my name to the list

---
+++ peoples/mrobinson.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Marcus Robinson',
+    github: 'MarcusRobinson928',
+    email: 'mrobinson928@hotmail.com',
+    twitter: '@',
+    facebook: 'Marcus Robinson',
+    linkedin: 'in/marcusrobinson'
+  }
