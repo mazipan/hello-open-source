4881128495ac35d09d0bde938e05bc6eaa0a4672
Niyaz <mohamedniyazm7@gmail.com>
2018-11-20 14:52:57 +0530

    niyaz

---
+++ peoples/niyaz2498.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'M.Mohamed niyaz',
+    github: 'niyaz2498',
+    email: 'icantsay@icantsay.com',
+    twitter: 'icantsay',
+    facebook: 'icantsay',
+    linkedin: 'in/icantsay'
+  }
