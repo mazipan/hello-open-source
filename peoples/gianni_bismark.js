00faa9763beeac29c6faeaf6bbd9e5d764dd5ae7
federico.cosentino <federico.cosentino@gmail.com>
2018-12-20 17:30:08 +0100

    person added

---
+++ peoples/gianni_bismark.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'Gianni Bismark',
+    github: 'gianni bismark',
+    email: 'giannibismark@gmail.com',
+    twitter: '@gianni_bismark',
+    facebook: 'Gianni.Bismark',
+    linkedin: 'in/giannuI_bismark'
+  }
+  
