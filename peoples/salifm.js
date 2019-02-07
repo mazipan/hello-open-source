65bc271e0e8da7ee889ac4bfbd3f3e98290fa077
Salif Mehmed <salif@programmer.net>
2019-02-08 01:59:58 +0200

    Create salifm.js

---
+++ peoples/salifm.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Salif Mehmed',
+  github: 'https://github.com/salifm',
+  email: 'salif@programmer.net',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/xxx'
+}
