60e8712b1f5ed60d5b96509ff09a44f1f9dd7d11
Ankur Kalambe <AK1915@live.mdx.ac.uk>
2019-04-15 21:09:13 +0100

    Create ankurkalambe.js

---
+++ peoples/module.exports = {   name: 'sharad',   github: 'XXX',   email: 'xxx@xxx.com',   twitter: '@xxx',   facebook: 'xxx',   linkedin: 'in/ankurkalambe.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'AnkurKalambe',
+  github: 'https://github.com/ankurkalambe',
+  email: 'AK1915@live.mdx.ac.uk',
+  twitter: '-',
+  facebook: 'Ankur Kalambe',
+  linkedin: 'in/ankur_kalambe'
+}
