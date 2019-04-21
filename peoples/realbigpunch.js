480cf3784aa6721a2bbf4ee0b407d5c7367ca1fa
RealBigpunch <34137527+RealBigpunch@users.noreply.github.com>
2019-04-21 15:30:10 +0100

    Create realbigpunch.js (#58)

---
+++ peoples/realbigpunch.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Umut Ekerdiker',
+  github: 'https://github.com/RealBigpunch',
+  email: 'umutekerdiker@gmail.com',
+  twitter: '---',
+  facebook: '---',
+  linkedin: '---'
+}
