a09ec796014d64fba393e385043063743a9a859a
Mark Underwood <mark.t.underwood@protonmail.com>
2019-05-01 23:17:01 -0800

    Create robinthesprite.js (#59)

---
+++ peoples/robinthesprite.js
@@ -0,0 +1,9 @@
+
+module.exports = {
+	  name: 'RobinTheSprite',
+	  github: 'https://github.com/RobinTheSprite',
+	  email: 'does.not.exist@fakemail.com',
+	  twitter: 'N/A',
+	  facebook: 'Robin Goodfellow',
+	  linkedin: 'N/A'
+}
