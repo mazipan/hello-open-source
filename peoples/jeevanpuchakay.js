1ebdd1f59357ceb9448419618b4269b8be60af11
jeevan <jeevangkr37@gmail.com>
2019-07-15 15:32:32 +0530

    Addition of jeevanpuchakay.js file

---
+++ peoples/jeevanpuchakay.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Jeevan',
+  github: 'https://github.com/jeevanpuchakay',
+  email: 'jeevangkr37@gmail.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/xxx'
+}
