7a6d6ebb2249f5482a35cfadd761a3e0a81ad682
rohanmd07 <41102695+rohanmd07@users.noreply.github.com>
2019-09-03 21:39:37 +0530

    Created github_rohanmd07.js
    Added my name

---
+++ peoples/github_rohanmd07.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Aaron Stone',
+  github: 'rohanmd07',
+  email: 'charlie.landers07@gmail.com',
+  twitter: '@aaronstone',
+  facebook: 'aaronstone',
+  linkedin: 'in/aaronstone'
+}
