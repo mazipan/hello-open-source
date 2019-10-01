e1fb41f199838dc55e0a003dd2d0e962542c6b68
santosidauruk <santosidauruk@gmail.com>
2019-10-01 18:25:39 +0700

    add santosidauruk account

---
+++ peoples/github_santosidauruk.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Santo Sidauruk',
+    github: 'santosidauruk',
+    email: 'santosidauruk@gmail.com',
+    twitter: '@santosidauruk',
+    facebook: 'santovincensius.sidauruk',
+    linkedin: 'in/santo-sidauruk/'
+  }
