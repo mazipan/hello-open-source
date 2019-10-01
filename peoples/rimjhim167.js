d91261ddd249de350219fafc8fb44d783ce709b0
Rimjhim Agrawal <rimjhimagrawal167@gmail.com>
2019-10-01 22:36:30 +0530

    added rimjhim167

---
+++ peoples/rimjhim167.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Rimjhim Agrawal',
+    github: 'rimjhim167',
+    email: 'rimjhimagrawal167@gmail.com',
+    twitter: '@RimjhimAg',
+    facebook: 'Rimjhim.Agrawal',
+    linkedin: 'RimjhimAgrawal'
+  }
