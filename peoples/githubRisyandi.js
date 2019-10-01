9dd567254648c72dc5b6971f419b624c31926795
Risyandi <Risyandi.94@gmail.com>
2019-10-01 13:49:46 +0700

    add new people (#72)

---
+++ peoples/githubRisyandi.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Risyandi',
+    github: 'Risyandi',
+    email: 'hello@Risyandi.com',
+    twitter: '@Risyandi94',
+    facebook: 'Risyandi.94',
+    linkedin: 'Risyandi94'
+}
