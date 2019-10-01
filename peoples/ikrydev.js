ce7773553d0071300ceac1fb7c0c986d62d451e8
Agus Fikri Suyuthi <40247655+ikrydev@users.noreply.github.com>
2019-10-01 20:34:45 +0700

    Add my Data (#75)

---
+++ peoples/ikrydev.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'ikrydev',
+    github: 'ikrydev',
+    email: 'ikrydev@gmail.com',
+    twitter: '@ikrydev',
+    facebook: 'ikrydev',
+    linkedin: 'in/ikrydev'
+  }
