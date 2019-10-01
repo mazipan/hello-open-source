6c081c5515963442673b5fdb6cc1a1046716b8ea
Abdul Muhit <abdul.muhit.t@gmail.com>
2019-10-01 20:59:01 +0700

    adding my data (#74)

---
+++ peoples/github_abdulMuhit.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Abdul Muhit',
+    github: 'abdulMuhit',
+    email: 'abdul.muhit.t@gmail.com',
+    twitter: '@Abdul_Muhit_T',
+    facebook: 'abdul.muhit.9',
+    linkedin: 'in/abdul-muhit-43bb3b67'
+  }
