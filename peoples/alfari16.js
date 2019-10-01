8a4226366263bb93fa5dfac3430beac997fd357a
alfari16 <alif.anshory@yahoo.com>
2019-10-01 23:35:43 +0700

    added alfari16

---
+++ peoples/alfari16.js
@@ -0,0 +1,15 @@
+module.exports = {
+  name: 'Alif Irfan Anshory',
+  github: 'alfari16',
+  email: 'alif.irfan16@gmail.com',
+  twitter: '@alif_irfan16',
+  linkedin: 'https://www.linkedin.com/in/alifirfananshory/',
+  skills: [
+    'vue.js',
+    'react.js',
+    'node.js',
+    'codeigniter',
+    'laravel',
+    'react native'
+  ]
+}
