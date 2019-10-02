17ec745072864341b97b169fd6c5bc6e1fca9172
Eka Sapta <ekasapta747@gmail.com>
2019-10-02 18:52:20 +0800

    adding ekasapta.js

---
+++ peoples/ekasapta.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Eka Sapta',
+    github: 'ekasapta',
+    email: 'xxx@xxx.com',
+    twitter: '@ekasapta747',
+    facebook: 'ekasapta17',
+    linkedin: 'in/ekasapta'
+}
