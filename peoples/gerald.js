51ddf2aa86c49fb760faf9662633e2c71d920b9d
Gerald Halomoan Samosir <gerald.unforgiven@gmail.com>
2019-10-03 17:07:47 +0700

    add user gerald (#82)

---
+++ peoples/gerald.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Gerald Halomoan Samosir',
+  github: 'geraldsamosir',
+  email: 'geralddeveloper95@gmail.com',
+  twitter: '@gerald_cuy',
+  facebook: 'geraldhalomoan.u.samosir',
+  linkedin: 'in/gerald-halomoan-samosir-34b11611b'
+};
