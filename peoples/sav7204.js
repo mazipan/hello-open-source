3d9e625b200db427ef0992e43e1007ea249e983a
sav7204 <55633354+sav7204@users.noreply.github.com>
2019-10-05 14:45:54 +0100

    Created sav7204.js
    Created sav7204.js and place in file People/

---
+++ peoples/sav7204.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Savethan Sritharan',
+  github: 'https://github.com/sav7204',
+  email: 'savethanorigin@hotmail.com',
+  twitter: '@sav7204',
+  facebook: '-----',
+  linkedin: '-----'
+}
