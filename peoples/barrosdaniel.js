212279a39a9f239e02dd08d03a00953113d04317
Daniel Barros <barros.daniel@outlook.com>
2019-10-05 13:51:04 +0800

    feature: Add barrosdaniel profile

---
+++ peoples/barrosdaniel.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Daniel Barros',
+  github: 'https://github.com/barrosdaniel',
+  email: 'barros.daniel@outlook.com',
+  twitter: '@BarrosDanielM',
+  facebook: 'https://www.facebook.com/profile.php?id=1337257008',
+  linkedin: 'https://www.linkedin.com/in/barros-daniel/'
+}
