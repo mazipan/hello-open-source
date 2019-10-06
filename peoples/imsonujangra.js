d14376526b90b5641be6167e91159e41856e3ada
Sonu Jangra <sonu.jangra@cardekho.com>
2019-10-06 10:57:26 +0530

    add my profile to people directory

---
+++ peoples/imsonujangra.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Sonu Jangra',
+  github: 'imsonujangra',
+  email: 'imsonujangra@yahoo.com',
+  twitter: 'imsonujangra',
+  facebook: 'sjangra_fb',
+  linkedin: 'sjangra'
+}
