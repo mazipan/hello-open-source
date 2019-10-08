9b5a9d2e87f0986e13e07d8c1082f74ff91ab1a2
DikshaKumari99 <53017825+DikshaKumari99@users.noreply.github.com>
2019-10-08 08:46:23 +0530

    added my details (#107)

---
+++ peoples/diksha.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Diksha Kumari',
+    github: 'dikshakumari99',
+    email: 'dikshakumari188@gmail.com',
+    twitter: '-na-',
+    facebook: 'Diksha Kumari',
+    linkedin: 'in/dikshak'
+  }
