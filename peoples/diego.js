70a7738f08c91d1c7b2ed627f02632846f4e9341
Diego Rondão <drondao@autodoc.com.br>
2019-10-08 10:47:53 -0300

    Update Added my file

---
+++ peoples/diego.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Diego Etelvaldo',
+  github: 'EtelvaldoDev',
+  email: 'etelvaldo.Dev@gmail.com',
+  twitter: 'etelvaldo.Dev',
+  facebook: 'etelvaldo.Dev',
+  linkedin: 'in/etelvaldo.Dev/'
+}
