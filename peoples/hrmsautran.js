94896f04d7bf930f8b2060ef3ce01ec48dc4995a
Hermes Autran <hermes.autran@gmail.com>
2019-10-07 23:14:37 -0400

    contributing to hacktoberfest, add my info to hrmsautran.js (#102)

---
+++ peoples/hrmsautran.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Hermes Autran',
+    github: 'hrmsautran',
+    email: 'hermes.autran@gmail.com',
+    twitter: 'hrmsautran',
+    facebook: 'none',
+    linkedin: 'in/hermes-autran-60087473/'
+  }  
