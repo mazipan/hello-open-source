b0ad2764fbf8a066bb4f14233e308ec874a76f0b
TomFrink <47752720+TomFrink@users.noreply.github.com>
2019-10-08 22:40:21 -0400

    Added Tom's File (#122)

---
+++ peoples/TomFrink.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'TomFrink',
+    github: 'TomFrink',
+    email: 'TomFrink@IdRatherNot.sorry', 
+    twitter: '@TomFrink',
+    facebook: 'Search for me',
+    linkedin: 'https://www.linkedin.com/in/tomfrink/'
+  }
+  
