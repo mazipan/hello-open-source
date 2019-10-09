a9738ff0ea9a35721274b710a3a08cb1427d7e71
Deta <detautama11@gmail.com>
2019-10-09 23:39:59 +0800

    Adding detautama

---
+++ peoples/detautama.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'detautama',
+  github: 'https://github.com/detautama',
+  email: 'detautama11@gmail.com',
+  twitter: 'https://twitter.com/UtamaDeta',
+  facebook: 'facebook.com/putudeta11',
+  linkedin: 'https://www.linkedin.com/in/deta-u-8bb1a7107/'
+}
