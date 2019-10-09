499824a0450270986f32d174d9aceb50b621d7fd
Imam Aris Munandar <imamarisdeveloper@gmail.com>
2019-10-09 23:11:50 +0700

    Adding File : imamaris.js in list (#123)
    Adding file imamaris.js for open source hacktoberfest 2019

---
+++ peoples/imamaris.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Imam Aris Munandar',
+  github: 'imamaris',
+  email: 'imamarisdeveloper@gmail.com',
+  twitter: '@imamaris',
+  facebook: 'imam aris munandar',
+  linkedin: 'in/imamarismunandar/'
+}
