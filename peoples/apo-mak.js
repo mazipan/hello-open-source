3773e86d20458b9a5dde0da239211a3e6971150a
apo-mak <apo-mak@users.noreply.github.com>
2019-10-09 05:40:44 +0300

    Add apo-mak.js file (#121)

---
+++ peoples/apo-mak.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'apo-makE',
+    github: 'apo-mak',
+    email: 'apo-mak@users.noreply.github.com',
+    twitter: 'N/A',
+    facebook: 'N/A',
+    linkedin: 'N/A'
+  }
