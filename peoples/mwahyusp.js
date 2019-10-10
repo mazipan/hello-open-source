cae1dca1867b58e04bdd0c6ee4277ea69785daa2
Mohammad Wahyu Sanusi Putra <[mwahyu73@gmail.com]>
2019-10-10 10:44:58 +0700

    add-my-profile

---
+++ peoples/mwahyusp.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Mohammad Wahyu Sanusi Putra',
+  github: 'mwahyusp',
+  email: 'mwahyu73@gmail.com',
+  twitter: '@psuyhawm',
+  facebook: 'mowahyusapu',
+  linkedin: 'in/mwahyusp'
+}
