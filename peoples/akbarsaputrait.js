3d884178a8df51210377834628bc45f6ee143606
akbarsaputrait <akbarsaputrait@outlook.com>
2019-10-10 10:24:40 +0700

    Create akbarsaputrait.js

---
+++ peoples/akbarsaputrait.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Akbar Anung Yudha Saputra',
+  github: 'https://github.com/akbarsaputrait',
+  email: 'akbarsaputrait@outlook.com',
+  twitter: '@akbarsaputrait',
+  facebook: '/yudhaweb',
+  linkedin: ''
+}
