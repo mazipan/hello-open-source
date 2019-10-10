e712ee1706ccc9e68c2ab8e375bcde9371d25951
Julian Merida <julianmr97@gmail.com>
2019-10-10 10:09:11 -0300

    Add people/JukMR.js

---
+++ peoples/add-JukMR.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Julian Merida',
+  github: 'github.com/JukMR',
+  email: 'myawesomemail@gmail.com',
+  twitter: 'myAwesomeTwitterAcount',
+  facebook: 'myAwesomeFacebookAcount',
+  linkedin: 'in/julianmerida'
+}
