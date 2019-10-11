2e2e2644d0ae589139274e080655050f609f1dcc
Messias Henrique <40000015+messias-henrique@users.noreply.github.com>
2019-10-10 21:07:41 -0300

    Add Messias Henrique

---
+++ peoples/messias-henrique.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Messias Henrique',
+  github: 'messias-henrique',
+  email: 'messiashenrique2@gmail.com',
+  twitter: '@miciazhenrique',
+  facebook: '',
+  linkedin: ''
+}
