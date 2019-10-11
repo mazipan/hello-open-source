d7161783799aeef35bc2d8e44dcdf37066503cdf
Sigit Setiaji <setiaji24@gmail.com>
2019-10-11 14:45:41 +0700

    Create sigitwp.js (#134)

---
+++ peoples/sigitwp.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Sigit Setiaji',
+  github: 'sigitwp',
+  email: 'setiaji24@gmail.com',
+  facebook: 'https://www.facebook.com/mas.sigit.58367116',
+  linkedin: 'www.linkedin.com/in/sigit-setiaji-9b41828a'
+}
