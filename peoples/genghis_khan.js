8b8e04379831b7a23d7866d18992beaf8d3f57a0
sanbaid <sanbaid31@gmail.com>
2019-10-11 18:30:00 -0700

    Added Genghis Khan

---
+++ peoples/genghis_khan.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Genghis Khan',
+    github: 'github.test/g-khan',
+    email: 'genkhan@test.com',
+    twitter: 'i_am_khan',
+    facebook: 'genghis_and_friends',
+    linkedin: 'in/theKhan'
+  }
