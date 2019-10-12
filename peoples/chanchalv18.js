98711be53a2386eab3151fa3fb5de946118a6b27
chanchalv18 <chanchals.vish1828@gmail.com>
2019-10-12 22:18:44 +0530

    Create chanchalv18.js

---
+++ peoples/chanchalv18.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Chanchal V',
+  github: 'chanchalv18',
+  email: 'chanchalv187@gmail.com',
+  twitter: '@ChanchalVishw16',
+  facebook: 'xxx',
+  linkedin: 'in/https://www.linkedin.com/in/chanchal-vishwakarma-180797/'
+}
