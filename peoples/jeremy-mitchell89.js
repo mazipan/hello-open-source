1c79f5aa6d8cb7c797fc74df3aff38defb60e210
Jeremy Mitchell <jeremy.mitchell0627@gmail.com>
2019-10-12 23:46:15 -0500

    Added jeremy-mitchell89.js (#147)

---
+++ peoples/jeremy-mitchell89.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Jeremy',
+  github: 'https://github.com/Jeremy-Mitchell89',
+  email: 'jeremy.mitchell0627@gmail.com',
+  twitter: '',
+  facebook: '',
+  linkedin: ''
+}
