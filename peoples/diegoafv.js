e4e38ba61e79c8f7b72054a8e340801d88da9512
Diego Ferreira <dafvus@gmail.com>
2019-10-13 15:55:06 -0700

    add diegoafv.js

---
+++ peoples/diegoafv.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Diego Ferreira',
+    github: 'Diegoafv',
+    email: 'diego@gmail.com',
+    twitter: '@diegoafv',
+    facebook: 'diegoafv',
+    linkedin: 'in/diegoafv/'
+}
