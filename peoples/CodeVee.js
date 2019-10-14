59e85a36856b284369b222e4d2dfc9b790ae98a4
Victor <53838340+CodeVee@users.noreply.github.com>
2019-10-14 17:22:29 +0100

    Create CodeVee.js

---
+++ peoples/CodeVee.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'VictorAbel',
+    github: 'CodeVee',
+    email: 'talk2vee@yahoo.com', 
+    twitter: '@victor5432',
+    facebook: 'Victor Starboy',
+    linkedin: 'https://www.linkedin.com/in/victorAbel/'
+  }
