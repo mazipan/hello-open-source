a10a7aa6f5e5cb8c7812c7a8c77f52499a9bc3de
Magnus Paulsen <magnusp99@hotmail.com>
2019-10-14 18:56:44 +0200

    added powni.js to peoples

---
+++ peoples/powni.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Powni',
+  github: 'https://github.com/powni',
+  email: 'kakkawha17@hotmail.com',
+  twitter: '@powni',
+  facebook: 'powni',
+  linkedin: 'in/powni'
+}
