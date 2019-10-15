fc45fd5eff3ef9a9fcf156c9defbd28c26bb70b1
Mai Amundsen <51907639+mai-am@users.noreply.github.com>
2019-10-15 03:25:03 +0200

    Create mai-am.js (#154)
    Add new person to people.

---
+++ peoples/mai-am.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'ThisIsMe',
+    github: 'mai-am',
+    email: 'betyouwantto@mail.me', 
+    twitter: '@RealMai-Am',
+    facebook: 'Real Mai-Am',
+    linkedin: 'https://www.linkedin.com/in/somegenious/'
+  }
