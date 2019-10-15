0feb2ea0b22a714eca99eed7bc446247cea3b87b
Gabriela Lima <gabirlima@gmail.com>
2019-10-14 22:18:21 -0300

    add gabirlima (#162)

---
+++ peoples/gabrielalima.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Gabriela Lima',
+  github: 'gabirlima',
+  email: 'gabirlima@gmail.com',
+  twitter: '@CravoCanelaLima',
+  facebook: 'gabirlima',
+  linkedin: 'in/gabriela-lima-3189a821/'
+}
