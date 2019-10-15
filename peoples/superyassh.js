933bdad906924c9e1ee9c8240776951c56da6e7f
Yanshu Kishore <53146064+superyassh@users.noreply.github.com>
2019-10-15 06:56:19 +0530

    Added my Detail (#155)
    Created superyassh.js in peoples/ directory and added my Detail in that.

---
+++ peoples/superyassh.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'Yanshu Kishore',
+    github: 'superyassh',
+    email: 'yanshukishore@gmail.com',
+    twitter: 'spr_yash',
+    facebook: 'superyassh',
+    linkedin: 'in/superyassh/'
+  }
+  
