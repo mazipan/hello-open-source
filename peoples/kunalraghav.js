f515c892f2ff76dbec5d22d5c44c9eb74d340c3f
KunalRaghav <kraghav123@gmail.com>
2019-10-16 21:16:03 +0530

    added details

---
+++ peoples/kunalraghav.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Kunal Raghav',
+  github: 'https://github.com/KunalRaghav/',
+  email: 'kraghav123@gmail.com',
+  twitter: '@_Kunal_Raghav_',
+  facebook: 'Kunal Raghav',
+  linkedin: 'in/kunalraghav/'
+}
