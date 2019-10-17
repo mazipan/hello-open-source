69001c7271927ed2266a67af6f4db95c3dc08d3a
Mohit Khemchandani <42883046+mohitkhemchandani@users.noreply.github.com>
2019-10-17 06:33:13 +0530

    Create github_mohitkhemchandani.js (#167)

---
+++ peoples/github_mohitkhemchandani.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Mohit_Khemchandani',
+  github: 'https://github.com/mohitkhemchandani',
+  email: 'cryptonic007@protonmail.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/xxx'
+}
