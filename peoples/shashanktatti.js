dc579313fc7209e1f2822e06170c383b2a70bdd6
Shashank TD <tattishashank@gmail.com>
2019-10-17 06:33:32 +0530

    Created shashanktatti.js (#168)

---
+++ peoples/shashanktatti.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'SHASHANK T D',
+    github: 'https://github.com/shashanktatti',
+    email: 'sanushaan89@gmail.com',
+    twitter: '@xxx',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+  }
