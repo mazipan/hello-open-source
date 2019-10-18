e88d91f36bf547e5aea78b5d1911ad96f31ed61b
Sumeet Panjabi <sumeet.panjabi@vuclip.com>
2019-10-18 14:38:09 +0530

    Add identity profile

---
+++ peoples/github_sumeetpanjabi.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Sumeet Panjabi',
+  github: 'sumeetpanjabi',
+  email: 'sumeet.panjabi@vuclip.com',
+  twitter: '@sumeet_panjabi',
+  facebook: 'sumeetpanjabi',
+  linkedin: 'in/sumeet-panjabi'
+}
