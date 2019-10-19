c15f1e805131067026db8c5ef720ec69c477ecd7
Yogesh Gupta <Yogesh8435661391@gmail.com>
2019-10-19 20:38:01 +0530

    Create Yg12061998.js (#179)

---
+++ peoples/Yg12061998.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Yogesh Gupta',
+  github: 'github.com/Yg12061998',
+  email: 'yogesh8435661391@gmail.com',
+  twitter: 'Yogesh1206',
+  facebook: 'Yogi1206',
+  linkedin: 'in/Yogi1206'
+}
