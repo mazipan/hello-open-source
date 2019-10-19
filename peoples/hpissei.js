8a605ba5883735313e8c4454a17bb0f4b78314ed
Heetesh Panghanti <32298685+hpissei@users.noreply.github.com>
2019-10-19 20:39:09 +0530

    Add hpissei (#182)

---
+++ peoples/hpissei.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'hpissei',
+  github: 'hpissei',
+  email: 'hpultimatemedia@gmail.com',
+  twitter: '@hpultimatemedia',
+  facebook: 'hpultimatemedia',
+  linkedin: 'in/hpultimatemedia'
+}
