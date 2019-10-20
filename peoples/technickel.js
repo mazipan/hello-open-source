c5fcd983ab8499a48af7995b943675e71b8bed0d
Alexander Bourlotos <alexander.bourlotos@gmail.com>
2019-10-19 20:59:13 -0500

    Added name to repo

---
+++ peoples/technickel.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Tech Nickel',
+    github: 'technickel',
+    email: 'tech@nickel.com',
+    twitter: '@alexbourlotos',
+    facebook: 'none',
+    linkedin: 'none'
+  }
