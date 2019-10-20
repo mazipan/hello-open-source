1c2767a1994c4829014a4946267a0146cecd25cf
rithgan <rithikgandhi1989@gmail.com>
2019-10-20 15:11:20 +0530

    Add rithgan.js to peoples

---
+++ peoples/rithgan.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Rithik Gandhi',
+    github: 'rithgan',
+    email: 'goodfeel07@gmail.com',
+    twitter: '@GandhiRithik',
+    facebook: 'Rithk Gandhi',
+    linkedin: 'rithik-gandhi'
+  }
