13dfb877f690259c86686ca14d3635a1f5df1968
Rishabh000 <56599651+Rishabh000@users.noreply.github.com>
2019-10-22 00:21:51 +0530

    My info

---
+++ Rishabh000.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Rishabh Ranka',
+  github: 'Rishabh000',
+  email: 'rishabh_ranka@yahoo.com',
+  facebook: 'rishabh.ranka.942',
+  linkedin: 'in/rishabh-ranka-876b21186/'
+}
