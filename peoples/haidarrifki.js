fb0ba9f790643b5942e88d99424f549eeaaf06c7
Haidar Rifki <haidarrfki@gmail.com>
2019-10-21 09:49:12 +0700

    added new peoples (#187)

---
+++ peoples/haidarrifki.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Haidar Rifki',
+    github: 'haidarrifki',
+    email: 'haidarrfki@gmail.com',
+    twitter: '@haidarrifki',
+    facebook: 'Haidar Rifki',
+    linkedin: 'in/haidarrifki'
+}
