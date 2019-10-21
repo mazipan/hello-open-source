682e3a9814910be7473b3a357b57a7f634d04a53
Alexander Wayne Omorokunwa <xeroxism@gmail.com>
2019-10-21 08:04:02 +0100

    created xeroxism.js

---
+++ peoples/xeroxism.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Aragon',
+  github: 'https://github.com/aragon',
+  email: 'aragon1t3@gmail.com',
+  twitter: '@aragon1t3',
+  facebook: 'aragon1t3',
+  linkedin: 'in/aragon1t3'
+}
