7435f9f28bb40ae3cbb9409730e49cae07846d29
Rahul Kumar Shah <rahulkumarshah5000@gmail.com>
2019-10-20 19:48:56 -0700

    Add my name rahulshah456.js to peoples (#189)

---
+++ peoples/rahulshah456.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Rahul Shah',
+    github: 'rahulshah456',
+    email: 'awesome@example.vom',
+    twitter: '@Ghost',
+    facebook: 'Rahul Shah',
+    linkedin: 'Rahul Shah'
+  }
