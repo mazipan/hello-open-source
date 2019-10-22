5c8fee35e7e7dca8fd7320743d1cea1094b67f4f
Angga Dwi Arifandi <namapanjanggua@gmail.com>
2019-10-22 20:44:56 +0700

    add blackmenthor js (#201)

---
+++ peoples/blackmenthor.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Angga Arifandi',
+    github: 'blackmenthor',
+    email: 'anggadwiarifandi96@gmail.com',
+    twitter: '@anggadwia_',
+    facebook: 'Angga Dwi Arifandi',
+    linkedin: 'in/angga-dwi-arifandi-6414b8b8'
+}
