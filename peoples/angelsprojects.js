128cb072e2da8b80b5f696ff466d546fa1af040c
Angel Arciniega <mangel.arciniega@gmail.com>
2019-10-21 20:01:38 -0700

    Create angelsprojects.js

---
+++ peoples/angelsprojects.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Angel Arciniega',
+    github: 'AngelsProjects',
+    email: 'mangel.arciniega@gmail.com',
+    twitter: 'none',
+    facebook: 'Angel Arciniega',
+    linkedin: 'in/arciniega-angel/'
+}
