9a9c03f19748b382bf99fe5073a6b405535ca4a7
bhattshubham15 <bhattshubham15@gmail.com>
2019-10-22 11:31:33 +0530

    first commit

---
+++ peoples/bhattshubham15.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'Shubham Bhatt',
+    github: 'github.com/bhattshubham15',
+    email: 'bhattshubham15@gmail.com',
+    facebook: 'Shubham Bhatt',
+    linkedin: 'https://www.linkedin.com/in/shubham-bhatt-b216ab148'
+  };
