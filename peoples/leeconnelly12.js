1778851578ada0895e98453217f4967e98f77af0
Lee Connelly <leeconnelly12@gmail.com>
2019-10-23 00:18:14 +0100

    added name

---
+++ peoples/leeconnelly12.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Lee',
+  github: 'https://github.com/LeeConnelly12',
+  email: 'leeconnelly12@gmail.com',
+  twitter: 'none',
+  facebook: 'none',
+  linkedin: 'none'
+};
