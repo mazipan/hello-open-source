f80332017f1acf3ec97052183091f98ebd246f45
poobest <poobest.pooh@gmail.com>
2019-10-23 13:29:59 +0700

    add_peoples

---
+++ peoples/hoilhong.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'hoikhong',
+    github: 'hidalgo-vntu',
+    email: 'hidalgo',
+    twitter: '@hidalgo',
+    facebook: 'hidalgo2005',
+    linkedin: 'hidalgo2005'
+  }
