c244207235a8599fae81fdba7a1c6bf052e65df8
Milind Chawre <milindchawre@gmail.com>
2019-10-23 22:03:55 +0530

    add-milind.js

---
+++ peoples/milind.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Milind Chawre',
+  github: 'milindchawre',
+  email: 'milindchawre@gmail.com',
+  linkedin: 'in/milind-chawre',
+  portfolio: 'https://milindchawre.github.io'
+}
