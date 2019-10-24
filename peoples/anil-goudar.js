9e366799d916813f5bb681e8e08025130c04704b
Anilkumar C Goudar <46928637+anil-goudar@users.noreply.github.com>
2019-10-24 05:09:59 -0400

    added user anil goudar (#226)

---
+++ peoples/anil-goudar.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'ANIL C GOUDAR',
+  github: 'anil-goudar',
+  email: 'anilgoudar@gmail.com',
+  twitter: '@goudaranil',
+  facebook: 'facebook.com/anilgoudar',
+  linkedin: 'in/goudar-anil'
+};
