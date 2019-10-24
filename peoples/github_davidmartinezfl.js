fd703398313f72085bcfa7bb27e13cf5bc330cec
David Martinez <davidmartinezfl@outlook.com>
2019-10-23 19:27:37 -0500

    [add] davidmartinezfl

---
+++ peoples/github_davidmartinezfl.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'David Martinez',
+  github: 'davidmartinezfl',
+  email: 'davidmartinezfl.mx@gmail.com',
+  twitter: '@davidmartinezfl',
+  facebook: 'davidmartinezfl',
+  linkedin: 'in/davidmartinezfl'
+};
