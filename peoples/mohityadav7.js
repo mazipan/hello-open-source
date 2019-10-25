cb9ed607727cd0a2453bec92fb442640ad42d6a7
Mohit Yadav <mohityadav1921@gmail.com>
2019-10-25 14:29:56 +0530

    added mohityadav7

---
+++ peoples/mohityadav7.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Mohit Yadav',
+    github: 'mohityadav7',
+    email: 'mohityadav1921@gmail.com',
+    twitter: '@_mohit_yadav',
+    facebook: 'mohityadav21',
+    linkedin: 'in/mohityadav7'
+};
