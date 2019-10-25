01c9908149bb46a856373cc752b2e8de11d27826
telecus1 <antonio.jaime96@gmail.com>
2019-10-25 13:36:31 +0200

    New file created

---
+++ peoples/telecus1.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Antonio Jaime',
+  github: 'telecus1',
+  email: 'antalcalv@alum.us.es',
+  twitter: '@iwontgiveit',
+  facebook: 'I wont give it',
+  linkedin: 'in/ajaa'
+}
