42e40d63c1e1badd6974c3ef8ac3ab9423d7301a
mukeshprasad <mukesh970378@gmail.com>
2019-10-25 10:24:51 +0530

    nothing

--- peoples/mukesh.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Mukesh Prasad',
-  github: 'mukeshprasad',
-  email: 'mukesh9703@gmail.com',
-  twitter: '@mukesh9703@',
-  facebook: 'Mukesh Prasad',
-  linkedin: 'Mukesh Prasad'
-};
