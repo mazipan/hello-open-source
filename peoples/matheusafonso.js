39274e0885f9d43fdf94d5657c58e05d89d3c554
matheusafonsouza <matheusafonsouza@gmail.com>
2019-10-25 13:08:36 -0300

    commit

---
+++ peoples/matheusafonso.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Matheus Afonso de Souza',
+  github: 'Matheusafonsouza',
+  email: 'matheusafonsouza@gmail.com',
+  twitter: '@MatheusAfsouza',
+  facebook: 'Matheus Afonso',
+  linkedin: 'in/matheusafonso'
+}
