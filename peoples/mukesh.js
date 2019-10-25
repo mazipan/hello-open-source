88bf17b5bba7b98098eb0690d7d1852df984f0e4
mukeshprasad <mukesh970378@gmail.com>
2019-10-25 10:33:25 +0530

    Added my file (#237)

---
+++ peoples/mukesh.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Mukesh Prasad',
+  github: 'mukeshprasad',
+  email: 'mukesh9@gmail.com',
+  twitter: '@mukesh970378',
+  facebook: 'Mukesh Prasad',
+  linkedin: 'Mukesh Prasad'
+};
