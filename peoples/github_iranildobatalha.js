024e9c3e55fae27ce260d6933295a6c50de26d32
Iranildo <35541435+iranildobatalha@users.noreply.github.com>
2019-10-25 13:47:46 -0400

    My file

---
+++ peoples/github_iranildobatalha.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'iranildobatalha',
+  github: 'https://github.com/iranildobatalha',
+  email: 'iranildobatalha@gmail.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/xxx'
+};
