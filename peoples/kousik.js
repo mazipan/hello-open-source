088c6eb0de965e7758ee1efc2ce6cab118dcd293
Somala Koushik Reddy <47821295+godlysaiko@users.noreply.github.com>
2019-10-26 11:44:51 +0530

    Create kousik.js (#249)

---
+++ peoples/kousik.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'kousik reddy',
+    github: 'godlysaiko',
+    email: 'THEGOD@gmail.com',
+    twitter: '@GODLYSAIKO',
+    facebook: 'Koushik Reddy Somala',
+    linkedin: 'koushik Reddy'
+  }
