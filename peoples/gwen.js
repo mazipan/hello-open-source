7788981b407c0c719607e1288d5301d42d9643b3
mygwen <mygwen.glory@gmail.com>
2019-10-26 19:00:04 +0800

    add my profile to the list

---
+++ peoples/gwen.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Gwendolyne Maxine Stacy',
+    github: 'mygwen',
+    email: 'gwendolynemaxinestacy@gmail.com',
+    twitter: '@mygwen',
+    facebook: '',
+    linkedin: ''
+  }
