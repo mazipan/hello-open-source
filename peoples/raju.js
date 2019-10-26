c811bf26617b520e8c06b546b7a2d16028b31e4f
Rajueleti <57027673+Rajueleti@users.noreply.github.com>
2019-10-26 11:38:01 +0530

    Create raju.js

---
+++ peoples/raju.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'raju eleti',
+    github: 'rajueleti',
+    email: 'eraju12@gmail.com',
+    twitter: '@aezraju',
+    facebook: 'Raju Eleti',
+    linkedin: 'Eleti Raju'
+  }
