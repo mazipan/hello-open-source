7f104046740384ff0f109b8e47058fe202c81d24
charliemday <charliemday31@gmail.com>
2019-10-27 16:05:48 +0000

    Hello Open Source!

---
+++ peoples/charliemday.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Charlie',
+  github: 'charliemday',
+  email: 'charlie@hotmail.co.uk',
+  twitter: '@charliegithub',
+  facebook: 'charlie',
+  linkedin: 'in/charliegithub'
+};
