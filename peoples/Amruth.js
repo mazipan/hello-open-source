f20c2356e055f686a772c2fa287e20227c75c86b
amruth219 <57027156+amruth219@users.noreply.github.com>
2019-10-27 07:50:08 +0530

    Create Amruth.js (#252)

---
+++ peoples/Amruth.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Amruth Raj',
+  github: 'amruth219',
+  email: 'amruth.manchikanti@gmail.com',
+  twitter: '@this_is_amruth',
+  facebook: 'Amruth Raj',
+  linkedin: 'in/amruth-raj-manchikanti'
+};
