9265d6ed784fcbc22ea05c11070b1fd13c8f5b71
Eetu Eskelinen <edwardtehgreat@gmail.com>
2019-10-28 13:44:06 +0200

    Add eppu.js

---
+++ peoples/eppu.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Eetu Eskelinen',
+  github: 'eppu',
+  email: 'secret@mail.com',
+  twitter: '@edwardtehgreat',
+  facebook: 'not-applicable',
+  linkedin: 'in/eetueskelinen'
+};
