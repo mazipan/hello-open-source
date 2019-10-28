878bb02d620421f0cd04a9ea4afe4f502ef44b7c
Sulaiman-kc <sulaimankc9@gmail.com>
2019-10-28 16:37:18 +0530

    hacktober

---
+++ peoples/Sulaiman-kc.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'SULAIMAN KC',
+    github: 'Sulaiman-kc',
+    email: 'sulaimankc9@gmail.com',
+    twitter: '@xxx',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+  };
