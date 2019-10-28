c99994f35033ac14d565b72862a8bd4626f4ddc8
MedJawad <mohamedjawad.aatafay@gmail.com>
2019-10-28 21:46:23 +0100

    Add MedJawad.js To Project

---
+++ peoples/MedJawad.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Mohamed Jawad',
+  github: 'MedJawad',
+  email: 'mohamedjawad.aatafay@gmail.com',
+  twitter: '@mohamedJawad',
+  facebook: 'aatafay',
+  linkedin: 'in/mohamed-jawad-aatafay-219265167'
+};
