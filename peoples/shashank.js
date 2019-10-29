c0d1a825bdc2f2cdb6e8d433f65168361d2b478c
shashankvaidyar123 <57104701+shashankvaidyar123@users.noreply.github.com>
2019-10-29 11:18:54 +0530

    Update Bio (#260)

---
+++ peoples/shashank.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Shashank Vaidyar',
+  github: 'shashankvaidyar123',
+  email: 'shashankvaidyar1234@gmail.com',
+  twitter: '@shashankvaidyar',
+  facebook: 'Shashank Vaidyar',
+  linkedin: '#'
+};
