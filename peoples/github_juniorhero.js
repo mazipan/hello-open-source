c8373fed78e8961309759d14046e69c4dd0ae96a
Junior Hero <athuljayaram@gmail.com>
2019-10-30 16:19:04 +0530

    Create github_juniorhero.js (#264)

---
+++ peoples/github_juniorhero.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'ATHUL JAYARAM',
+  github: 'juniorhero',
+  email: 'athuljayaram@gmail.com',
+  twitter: '@athuljayaram',
+  facebook: 'iathul',
+  linkedin: 'in/athuljayaram'
+};
