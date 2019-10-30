3b6b754fa7ec13487fd18ce48c93d9e154e7c317
Vijay Gorfad <gorfadvijay1234@gmail.com>
2019-10-30 11:37:51 +0530

    Added myCard

---
+++ peoples/vijaygorfad.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "Vijay Gorfad",
+  github: "gorfadvijay",
+  email: "gorfadvijay1234@gmail.com",
+  twitter: "@gorfadvijay",
+  facebook: "gorfadv88",
+  linkedin: "in/vijaygorfad"
+};
