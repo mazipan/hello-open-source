01ca9092b86495e78c853803f613cb73567a0212
Rishabh000 <56599651+Rishabh000@users.noreply.github.com>
2019-10-30 16:19:44 +0530

    Add rishabh000.js (#266)
    * My info
    
    * Create rishabh.js

---
+++ peoples/rishabh.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'RISHABH_RANKA',
+  github: 'Rishabh000',
+  email: 'rishabh_ranka@yahoo.com',
+  facebook: 'Rishabh Ranka',
+  linkedin: 'in/rishabh-ranka-876b21186/'
+};
