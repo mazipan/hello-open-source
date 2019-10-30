0f8c00aedd64430195d723a65d25930f439b7017
Gustavo Silva <gustavoalprofissional@gmail.com>
2019-10-30 08:49:57 -0300

    Adding profile

---
+++ peoples/shadaw.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Gustavo Silva',
+  github: 'https://github.com/shadaw',
+  email: 'gustavoalprofissional@gmail.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/gustavo-alexandre-da-silva-08b672163'
+}
