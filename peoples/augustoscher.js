362f37030cb8ac6e8ebae55ab331c1859052696c
Augusto Scher <augusto.scher@resultadosdigitais.com.br>
2019-10-31 16:37:07 -0300

    adding my infos

---
+++ peoples/augustoscher.js
@@ -0,0 +1,6 @@
+module.exports = {
+  name: 'Augusto Scher',
+  github: 'https://github.com/augustoscher',
+  email: 'augustoscher@gmail.com',
+  linkedin: 'https://www.linkedin.com/in/augusto-scher/'
+};
