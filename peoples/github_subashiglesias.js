98e8bf38a15b4a5927217991b9131264e1b65c25
Subash P <subash.p@mrcooper.com>
2019-11-06 17:18:37 +0530

    [Added DarkLord to peoples]

---
+++ peoples/github_subashiglesias.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'DarKlord',
+  github: 'bellyofthebeast',
+  email: 'worldofwizards@evil.com',
+  twitter: '@darklord',
+  facebook: 'darklord',
+  linkedin: 'in/darklord'
+};
