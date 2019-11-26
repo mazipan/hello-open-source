bed915a25a4472faa0d591e2b4399bdc034da23b
Francis Njuguna Ng'ang'a <coulsorfrancois@yahoo.com>
2019-11-26 03:21:11 +0300

    Francis first open source contribution. (#284)

---
+++ peoples/Frankie0701145.js
@@ -0,0 +1,9 @@
+module.exports = {
+
+    name: 'Francis',
+    github: 'Frankie0701145',
+    email: 'coulsorfrancois@yahoo.com',
+    twitter: '@xxx',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+}
