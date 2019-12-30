fe9b8e882c3338f0fb2b7c3bd52106b69d1dfa32
greyGroot <grey.rsm@gmail.com>
2019-12-30 16:44:31 +0200

    greyGroot

---
+++ peoples/greyGroot.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'Sergii Rumiantsev',
+  github: 'greyGroot',
+  email: 'grey.rsm@gmail.com',
+  twitter: '',
+  facebook: 'https://www.facebook.com/profile.php?id=100023369408493',
+  linkedin: 'https://www.linkedin.com/in/sergey-rumiantsev-9826a0147/'
+
+}
