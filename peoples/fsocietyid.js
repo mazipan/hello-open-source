5cfb93a35dffcf325f889b5ce7126d4c0d42e919
Firman Abdul Jabar <firmanabduljabar@gmail.com>
2020-01-20 19:30:02 +0800

    :fire: fsocietyid (#286)

---
+++ peoples/fsocietyid.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'fsociety',
+    github: 'https://github.com/firmanjabar',
+    email: 'yayan6@hackermail.com',
+    twitter: '@firmanjabar',
+    facebook: 'firmanjabar',
+    linkedin: 'firman abdul jabar'
+}
