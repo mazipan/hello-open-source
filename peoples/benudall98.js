a0a69ad1cfb884bf2f461e2bc8ef982f62576551
Ben Ud <bu12@outlook.com>
2020-01-27 08:14:47 +0000

    Create benudall98.js (#292)

---
+++ peoples/benudall98.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'BEN_UDALL',
+  github: '2020',
+  email: 'boof@private.com',
+  twitter: '@whoevenusestwitter',
+  facebook: 'whoevenusesfb',
+  linkedin: 'in/warrenbuffet'
+};
