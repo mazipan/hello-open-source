bcd864fb1672d10fd65e972519167e512991ffa6
Irfan Maulana <mazipanneh@gmail.com>
2020-02-26 14:56:03 +0700

    Delete Rishabh000.js

--- Rishabh000.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  name: 'Rishabh Ranka',
-  github: 'Rishabh000',
-  email: 'rishabh_ranka@yahoo.com',
-  facebook: 'rishabh.ranka.942',
-  linkedin: 'in/rishabh-ranka-876b21186/'
-}
