d597265ba7628fcba9de27cc453aeb4deee3a0a1
Irfan Maulana <mazipanneh@gmail.com>
2020-02-26 14:50:18 +0700

    Update purge.js

--- purge.js
+++ purge.js
@@ -8,11 +8,14 @@ const peoplesPath = path.join(__dirname, "peoples");
 if (!namesToRemove.length) {
   fs.readdirSync(peoplesPath).forEach(file => {
     if (file.indexOf('mazipan.js') < 0) { 
-      fs.unlinkSync(path.join(peoplesPath, file))
+      try{
+        fs.unlinkSync(path.join(peoplesPath, file));
+        console.log(`Success deleting file: ${file}`);
+      }catch (e) {
+        console.error(`Error deleting file: ${file}`);
+      }
     }
   });
-
-  fs.rmdirSync(peoplesPath);
 } else {
   namesToRemove.forEach(name => {
     try {
@@ -21,6 +24,4 @@ if (!namesToRemove.length) {
       console.error(`Error deleting file for name: ${name}`);
     }
   });
-
-  fs.rmdirSync(peoplesPath);
 }
