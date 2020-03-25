7cbde38d6f4c5e17dcadb4eeaf3b8ee3a484eae9
Daniel <danieldeandradelopes@gmail.com>
2020-03-25 16:03:09 -0300

    added my code in test opensource

---
+++ peoples/testopensource.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "testopensource",
+  github: "testopensource",
+  email: "testopensource@testopensource.com",
+  twitter: "#test",
+  facebook: "test",
+  linkedin: "in/test"
+}
