8088688fda32b5851c14cf29650d860c4760bde3
Ripslash1 <64573361+Ripslash1@users.noreply.github.com>
2020-04-30 01:54:38 -0500

    Add files via upload (#318)

---
+++ peoples/Ripslash1.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Timothy Boseman',
+  github: 'Ripslash1',
+  email: 'toidle54@yahoo.com',
+  twitter: '@Ripslash1',
+  facebook: 'Timothy Boseman',
+  linkedin: 'N/A'
+};
