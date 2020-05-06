9da308067da7f9042cfecb5ab25a9ec274667fa0
mazipan <mazipanneh@gmail.com>
2020-05-06 14:03:10 +0000

    Purge data

--- peoples/Ripslash1.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Timothy Boseman',
-  github: 'Ripslash1',
-  email: 'toidle54@yahoo.com',
-  twitter: '@Ripslash1',
-  facebook: 'Timothy Boseman',
-  linkedin: 'N/A'
-};
