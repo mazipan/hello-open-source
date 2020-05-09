1a80ae9679523533f59dc26113ebe8920372a2fc
Sarah Price <48076969+SarehPrice@users.noreply.github.com>
2020-05-09 06:46:02 -0500

    Add github.sarehprice.js (#321)
    Thank you!

---
+++ github.sarehprice.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Sarah Price',
+  github: 'https://github.com/SarehPrice',
+  email: 'sarehprice@gmail.com',
+  twitter: '@SJPSnickers',
+  facebook: 'https://www.facebook.com/sarehprice',
+  linkedin: 'in/sarahprice0/'
+};
