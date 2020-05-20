8c3422e387acddc9515c292baf07f172a5e59509
Irfan Maulana <mazipanneh@gmail.com>
2020-05-20 13:36:36 +0700

    Delete github.sarehprice.js

--- github.sarehprice.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Sarah Price',
-  github: 'https://github.com/SarehPrice',
-  email: 'sarehprice@gmail.com',
-  twitter: '@SJPSnickers',
-  facebook: 'https://www.facebook.com/sarehprice',
-  linkedin: 'in/sarahprice0/'
-};
