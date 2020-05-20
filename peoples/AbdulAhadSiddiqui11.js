e8f2d9351f130df9e6d1e39771f5ac568a19115f
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2020-05-20 10:05:41 +0530

    Added AbdulAhadSiddiqui11 data!
    My first pull request!

---
+++ peoples/AbdulAhadSiddiqui11.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Abdul Ahad Siddiqui',
+  github: 'https://github.com/AbdulAhadSiddiqui11',
+  email: 'abdulahadsiddiqui011@gmail.com',
+  twitter: '@ImAbdulAhad11',
+  facebook: 'fb.com/AbdulAhadSiddiqui11',
+  linkedin: 'in/AbdulAhadSiddiqui11'
+};
