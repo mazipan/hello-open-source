f884c33a90224c10605b327cb0331fe70b22767c
Abdul Ahad Siddiqui <abdulahadsiddiqui011@gmail.com>
2020-05-20 09:55:06 +0530

    Create AbdulAhadSiddiqui11.js

---
+++ peoples/AbdulAhadSiddiqui11.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Abdul Ahad Siddiqui',
+  github: 'https://github.com/AbdulAhadSiddiqui11',
+  email: 'abdulahadsiddiqui011@gmail.com',
+  twitter: '@ImAbdulAhad',
+  facebook: 'fb.com/AbdulAhadSiddiqui11',
+  linkedin: 'in/AbdulAhadSiddiqui11'
+};
