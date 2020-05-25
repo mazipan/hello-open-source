5fd609f996550eb5bcb24fa77e3fd50ec3fc348a
Ambuj Singh <59581226+singhambuj@users.noreply.github.com>
2020-05-25 07:58:23 +0530

    Please merge my pull request (#325)
    * Add files via upload
    
    * Update singhambuj.js

---
+++ peoples/singhambuj.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'XYZ Example'
+  github: '@xyzexample',
+  email: 'example@gmail.com.com',
+  twitter: '@xyzexamle
+  facebook: 'xyzexaple',
+  linkedin: 'in/xyzexaple
+};
