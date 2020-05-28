f17c592c0fa6a89d3c25a61e23118a1ef2a9d884
Modou Lamin Jatta <laboyjatta@gmail.com>
2020-05-28 23:39:22 +0000

    My contribution (#327)

---
+++ peoples/jatta-njie21.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Modou  Lamin Jatta',
+    github: 'jatta-njie21',
+    email: 'laboyjatta@gmail.com',
+    twitter: '@HumbleViewer100',
+    facebook: 'laboy jatta',
+    linkedin: 'in/laboyjatta@gail.com'
+  };
