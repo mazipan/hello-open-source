e9716c567b94c5f14356a0e001fc8bd88367c777
Souvik48 <iamsouvikdas48@gmail.com>
2020-06-25 18:41:13 +0530

    Initial Comment

---
+++ peoples/github_Souvik48.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Souvik Das',
+  github: 'iamsouvik48@github.com',
+  email: 'souvikdummy@gmail.com',
+  twitter: 'souvik@twitter.com',
+  facebook: 'iamsouvikdas48@gmail.com',
+  linkedin: 'in/souvik-das-aa716a191/'
+};
