aa34c80234cd27cd1795590db2c3848eafad1fc6
Souvik48 <60789785+Souvik48@users.noreply.github.com>
2020-06-25 19:17:35 +0530

    Added a file inside /peoples (#328)
    * Initial Comment
    
    * second commit
    
    * final commit

---
+++ peoples/github_Souvik48.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Souvik',
+  github: 'iamsouvi148@github.com',
+  email: 'iamsouvikdasdummy@gmail.com',
+  twitter: 'souvik@twitter.com',
+  facebook: 'iamsouvikdas22@gmail.com',
+  linkedin: 'in/souvik-das/'
+};
