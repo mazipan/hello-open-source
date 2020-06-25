5c9d9551252d8391cc60c1567361dbb63d5d0071
Souvik48 <iamsouvikdas48@gmail.com>
2020-06-25 19:04:29 +0530

    final commit

--- peoples/github_Souvik48.js
+++ peoples/github_Souvik48.js
@@ -1,8 +1,8 @@
 module.exports = {
-  name: 'Souvik Das',
-  github: 'iamsouvik48@github.com',
+  name: 'Souvik',
+  github: 'iamsouvi148@github.com',
   email: 'iamsouvikdasdummy@gmail.com',
   twitter: 'souvik@twitter.com',
-  facebook: 'iamsouvikdas48@gmail.com',
-  linkedin: 'in/souvik-das-aa716a191/'
+  facebook: 'iamsouvikdas22@gmail.com',
+  linkedin: 'in/souvik-das/'
 };
