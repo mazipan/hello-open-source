d90082e3afda858bf3678f1f18c39f7ce38c4b39
mazipan <mazipanneh@gmail.com>
2020-07-01 14:00:54 +0000

    Purge data

--- peoples/github_Souvik48.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Souvik',
-  github: 'iamsouvi148@github.com',
-  email: 'iamsouvikdasdummy@gmail.com',
-  twitter: 'souvik@twitter.com',
-  facebook: 'iamsouvikdas22@gmail.com',
-  linkedin: 'in/souvik-das/'
-};
