e5fa64134786ed09ff71fb18d902c6ff8ff7ff29
Miftahul Lubab <31479825+babul28@users.noreply.github.com>
2020-08-31 18:47:52 +0700

    Create babul28.js (#334)
    Getting started to contribute on open source project

---
+++ peoples/babul28.js
@@ -0,0 +1,15 @@
+module.exports = {
+
+  name: 'bunglon',
+
+  github: 'babul28',
+
+  email: 'hambaAllah@gmail.com',
+
+  twitter: '@SiapaSaya',
+
+  facebook: 'Saya Siapa?',
+
+  linkedin: '👍'
+
+};
