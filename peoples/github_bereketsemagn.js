6bb89f8e7f035246fafe51ddaae214b69c777a02
Bereket <semagnbereket@gmail.com>
2020-09-18 20:56:30 -0400

    update

---
+++ peoples/github_bereketsemagn.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Bereket Semagn',
+    github: 'bereketsemagn',
+    email: 'bsemagn.dev@gmail.com',
+    twitter: 'test@xxx',
+    facebook: 'testxxx',
+    linkedin: 'test/xxx'
+  };
