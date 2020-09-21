769fee8e3f83b9def4560c2ce855bc6454dac598
Hendra Agil Syaputra <hendraaagil@gmail.com>
2020-09-21 19:58:27 +0700

    Create github_hendraaagil.js (#340)

---
+++ peoples/github_hendraaagil.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Hendra Agil',
+  github: 'hendraaagil',
+  email: 'hendraaagil@test.co.id',
+  twitter: '@hendraaagil',
+  facebook: 'hendraaagil',
+  linkedin: 'in/hendraaagil',
+};
