0484adf7f9e24281ce142d691e0d11e18e3b0501
Nivedita967 <66518355+Nivedita967@users.noreply.github.com>
2020-09-22 21:49:19 +0530

    nivedita

---
+++ peoples/github_nivedita.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'NIVEDITA SINGH',
+    github: 'https://github.com/Nivedita967',
+    email: 'nivedita19390@gmail.com',
+    twitter: 'https://twitter.com/Nivedit95025806',
+    facebook: '',
+    linkedin: ''
+  };
