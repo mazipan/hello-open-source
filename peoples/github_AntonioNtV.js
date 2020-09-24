e34649383128b6cf5b571996900c4f67747da67a
AntonioNtV <antoniontv18@gmail.com>
2020-09-24 15:30:05 -0300

    Adding AntonioNtV as people

---
+++ peoples/github_AntonioNtV.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Antonio Bertino de Vasconcelos Cabral Neto',
+    github: 'AntonioNtV',
+    email: 'antoniontv18@gmail.com',
+    twitter: '@antoniontv2',
+    facebook: '/facebook/AntonioNtV',
+    linkedin: 'in/antonio-bertino-80387118b'
+  }
