13a8c160daa01607537e85664ca6fcf6ea30a4f2
marianacoimbra <mariana.coimbra@ccc.ufcg.edu.br>
2020-09-27 07:53:18 -0400

    adding myself as person on this repository

---
+++ peoples/github_marianavoimbra.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: "Mariana Victoria",
+  github: "marianacoimbra",
+  email: "mariv.coimbra@gmail.com",
+  twitter: "",
+  facebook: "",
+  linkedin: "https://www.linkedin.com/in/mariana-coimbra-87087118b/",
+};
