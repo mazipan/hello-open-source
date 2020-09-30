d046787edc8a13074e5725192f988542dade9c1c
Muhamad Zainal Arifin <zainalarifin080718@gmail.com>
2020-10-01 03:58:02 +0700

    Create github_Zainal21.js

---
+++ peoples/github_Zainal21.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Muhamad Zainal Arifin',
+  github: 'Zainal21',
+  email: 'Zainalarifin080718@gmail.com',
+  twitter: '@Penulisduduk',
+  facebook: 'Muhammad Zain',
+  linkedin: 'in/muhammad-zainal-arifin-3092a1198',
+};
