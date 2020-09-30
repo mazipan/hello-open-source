685aa457c5d41f9b68ecd78aeb62b4a80714ec02
Sandip Roy <sandiproy2104@gmail.com>
2020-09-30 19:55:14 +0530

    my first contribution on github

---
+++ peoples/github_sandiproybalitha.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Sandip Roy',
+  github: 'sandiproybalitha',
+  email: 'testingpurpose@gmail.com',
+  twitter: 'don't use',
+  facebook: 'Sandiproy',
+  linkedin: 'don't use'
+}
