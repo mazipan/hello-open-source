7f5fc61153fb213d7d8dd4ccac4c3fe773ef8787
Abhishek Prasad <abhpdmail@gmail.com>
2020-10-01 16:39:33 +0530

    Added github_abhpd.js

---
+++ peoples/github_abhpd.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: "Abhishek Prasad",
+    github: "abhpd",
+    email: "prasadabhishek755@gmail.com",
+    twitter: "@abhpd",
+    facebook: "xxx",
+    linkedin: "in/abhpd",
+};
