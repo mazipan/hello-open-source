1a825ce9f27aff92aca6596c63914a75d6d09207
Abhra Kanti Dubey <58810632+abhrakantidubey@users.noreply.github.com>
2020-10-01 08:39:32 +0530

    Create github_abhrakantidubey.js (#354)

---
+++ peoples/github_abhra.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Abhra Kanti Dubey',
+  github: abhrakantidubey',
+  email: 'abhrakantidubey@gmail.com',
+  twitter: 'Don't use',
+  facebook: 'Don't use',
+  linkedin: 'Don't use',
+};
