44f17ed1490307ca1aaa24bf03f07e8366dfce51
Chiranjeet Mandal <chiranjeetmandal222@gmail.com>
2020-10-04 15:40:46 +0530

    Code with identity (#374)

---
+++ peoples/ChiranjeetMandal.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Chiranjeet_Mandal',
+  github: 'ChiranjeetMandal',
+  email: 'chiranjeetmandal@gmail.com',
+  twitter: '@chiranjeet680',
+  linkedin: 'https://www.linkedin.com/in/chiranjeet-mandal-37177a145/'
+};
