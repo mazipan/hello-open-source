ba93095bb2b664808f72fee5c07b82f268a86b52
Bayu Prakoso <bayuprakoso.adi@gmail.com>
2020-10-05 20:13:38 +0700

    add people/purakoso.js

---
+++ peoples/purakoso.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Bayu Adi Prakoso',
+  github: 'https://github.com/purakoso',
+  email: 'bayuprakoso.adi@gmail.com',
+  twitter: '@mbokdjah',
+  linkedin: 'in/purakoso'
+};
