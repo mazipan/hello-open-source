ed0ac74fcba3213959f2a0057e50965d604dbd9f
Rivaldi Putra <36479850+vadhe@users.noreply.github.com>
2020-10-05 23:19:11 +0800

    Create vadhe.js (#377)
    first commit

---
+++ peoples/vadhe.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'vadhe',
+  github: 'https://github.com/vadhe',
+  email: 'aldyvadhe@gmail.com',
+  twitter: 'https://twitter.com/vadhe_',
+  linkedin: 'https://bit.ly/linkedin-vdh'
+};
