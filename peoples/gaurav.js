b49ecb3b14a687d11d13f542a6f23a37a6ec1e4d
Gaurav Gupta <gauravguptagtm@gmail.com>
2020-10-06 18:49:59 +0530

    adding profile (#378)

---
+++ peoples/gaurav.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Gaurav',
+  github: 'github.com/gaurav-gupta-gtm',
+  email: 'itzme485@gmail.com',
+  twitter: '@gaurav',
+  facebook: 'facebook.com/gaurav',
+  linkedin: 'in/gaurav-gupta-gtm'
+};
