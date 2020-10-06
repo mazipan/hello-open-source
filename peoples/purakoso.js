47ff7917236888161a54ff965e63a88feb546137
Bayu Prakoso <bayuprakoso.adi@gmail.com>
2020-10-06 20:30:23 +0700

    update twitter username

--- peoples/purakoso.js
+++ peoples/purakoso.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'Bayu Adi Prakoso',
   github: 'https://github.com/purakoso',
   email: 'bayuprakoso.adi@gmail.com',
-  twitter: '@mbokdjah',
+  twitter: '@maskoso',
   linkedin: 'in/purakoso'
 };
