97afcdc233e6c8501b2ed4b6075f8d9ab1a09527
M Fariz Wisnu prananda <64114487+xvbnm48@users.noreply.github.com>
2020-10-14 15:01:20 +0700

    add me (#381)

---
+++ peoples/farizzz.js
@@ -0,0 +1,8 @@
+module.exports = {
+        name: 'farizzz',
+        github: 'Xvbnm48',
+        email: 'muhamadfarizwisnuuprananda@gmail.com',
+        twitter: '@jekeityplg',
+        facebook: 'fariz prananda',
+        linkedin: 'in/m-fariz-wisnu-prananda-05b28917b/'
+};
