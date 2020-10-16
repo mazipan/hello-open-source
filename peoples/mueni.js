1cc2a518dfe7a377a698515c21dc8de30b4e64bb
Esther Mueni <essymueni19@gmail.com>
2020-10-15 22:51:13 -0700

    Create mueni.js (#382)
    Just learning how to make OS contributions: first-timer

---
+++ peoples/mueni.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'mueni Mwaniki',
+  github: 'github.com/mueni',
+  email: 'mueni123@gmail.com',
+  twitter: '@mueni',
+  facebook: 'mueni Mwanik1',
+  linkedin: 'in/Mwaniki'
+};
