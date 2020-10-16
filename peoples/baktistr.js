9b3e386e022a2f1b61af8a4a7cd109ba4af4b523
unknown <bakti.satria.a@gmail.com>
2020-10-16 21:59:37 +0700

    Hello, salam kenal mazipan, seko wong pemalang

---
+++ peoples/baktistr.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Bakti',
+    github: 'baktistr',
+    email: 'bakti.satria.a@gmail.com',
+    twitter: '@baktistr',
+    facebook: 'baktistr',
+    linkedin: 'in/bakstistr'
+  };
