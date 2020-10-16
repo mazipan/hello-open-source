df65da544315c9a8955e080ad16c32ea7c804186
Hussain, Syed Muhammad Abid <syedmuhammadabid.h@alibaba-inc.com>
2020-10-16 13:14:42 +0500

    My Profile added

---
+++ peoples/syedmuhammadabid.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Syed Muhammad Abid',
+    github: 'https://github.com/syedmuhammadabid',
+    email: 'syedmuhammadabid640@gmail.com',
+    twitter: '@SyedAbidZaidi12',
+    facebook: 'syedmuhammadabid786',
+    linkedin: 'in/syedmuhammadabid'
+  };
