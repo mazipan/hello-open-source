5a4645e7270caf7d29ef778851d4dbeaae7197c8
aufal marom <aufalmarom@users.noreply.github.com>
2020-10-18 16:06:22 +0700

    try to contribute oss (#386)
    Co-authored-by: aufal.arrozi <aufal.arrozi@bankmandiri.co.id>

---
+++ peoples/aufalmarom.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'awfall',
+    github: 'aufalmarom',
+    email: 'aufalmarom23@gmail.com',
+    twitter: '@aufalmaromdotid',
+    facebook: 'aufalmarom',
+    linkedin: 'in/aufalmarom'
+  };
