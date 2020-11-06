7670f0c7d43fedfa5ca0027cf1e66f2ef638fcd9
trastanechora <trastanechora@gmail.com>
2020-11-06 17:27:08 +0700

    Add trastanechora.js

---
+++ peoples/trastanechora.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Mae',
+  github: 'trastanechora',
+  email: 'trastanechora@gmail.com',
+  twitter: '@trastanechora',
+  facebook: 'trastanechora',
+  linkedin: 'in/trastanechora'
+};
