6ede56ada74224b75f02a31abe307c4d478925fa
Asher Mathews <msasher123@gmail.com>
2021-01-16 15:13:13 +0530

    Create github_asherms.js

---
+++ people/github_asherms.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Monus',
+  github: 'monus123',
+  email: 'monus@jinkamail.com',
+  twitter: '@monusofficial',
+  facebook: 'monusrider',
+  linkedin: 'in/monusgoodboy'
+};
