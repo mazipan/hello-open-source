c20edcddd28ad4ce82f11d3c63b8641ae38ca53a
buhari44 <adeyemibuari@gmail.com>
2021-01-24 08:14:46 +0100

    Add my name

---
+++ people/github_adeyemiprofile.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Adeyemi Buhari',
+  github: 'buhari44',
+  email: 'adeyemibuari@gmail.com',
+  twitter: '@AdeyemiBuhari3',
+  facebook: 'Adeyemi Buhari',
+  linkedin: 'in/adeyemi-buhari-3aa4781b5/'
+}
