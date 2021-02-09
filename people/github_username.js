bdd460cebecc3a01e48b2fe2eb32516e1ea21e2b
Divyanshu Bisht <57855321+divyanshubisht@users.noreply.github.com>
2021-02-09 15:13:52 +0530

    Create github_username.js (#404)
    my details

---
+++ people/github_username.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Divyanshu',
+  github: 'https://github.comdekn',
+  email: 'bishdjdfowpil.com',
+  twitter: 'https://twitter.com/Divyaqddfs7882',
+  facebook: 'https:dfsdmfk;sd,02',
+  linkedin: 'indiaismy ovpdkf'
+};
