3be84a2f8dd26923438a346470ada29d5cb154c4
alfidh02 <alfidarmawan79@gmail.com>
2021-02-17 20:36:46 +0700

    add alfidh02 to people

---
+++ people/alfidh02.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Alfi Dharmawan',
+  github: 'https://github.com/alfidh02',
+  email: 'tryhardtommr@gmail.com',
+  twitter: 'https://twitter.com/fulan',
+  facebook: '@fulan.opensource',
+  linkedin: 'Fulan OS'
+};
