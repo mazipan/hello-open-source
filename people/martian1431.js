debe0c534602449eb635a2906b6f856504af7839
Phetho Macmillian Malope <pmalope@student.wethinkcode.co.za>
2021-02-28 01:49:03 +0200

    My first pull request on github. (#408)

---
+++ people/martian1431.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'martian',
+  github: 'martian1431',
+  email: 'martian@martian.com',
+  twitter: '@martian1431',
+  facebook: 'martian1431',
+  linkedin: 'in/martian1431'
+};
