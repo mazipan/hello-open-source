8bb112add8ac52d86b02dc12db515ab153a1f4b9
Jakub Konefał <59176401+JakubKonefal@users.noreply.github.com>
2021-03-10 17:08:51 +0100

    add github_username.js (#413)

---
+++ people/github_username.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Jakub',
+  github: 'https://github.com/JakubKonefal/',
+  email: 'barca01102@gmail.com',
+  twitter: 'no-twitter',
+  facebook: 'no-facebook',
+  linkedin: 'no-linkedin',
+};
