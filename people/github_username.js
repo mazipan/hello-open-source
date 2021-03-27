25af97bc72c03c63d2920b9f426b1882e726b9bb
Zhiguang Xu <xuzhiguang@gmail.com>
2021-03-27 18:15:29 -0400

    Added github_zx.js

--- people/github_username.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Hardik',
-  github: 'https://github.com/hardikh3/',
-  email: 'hardikh3@gmail.com',
-  twitter: 'no-twitter',
-  facebook: 'no-facebook',
-  linkedin: 'https://www.linkedin.com/in/hardik-hedaoo-32a03b16b/',
-};
