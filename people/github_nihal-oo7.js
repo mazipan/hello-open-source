11f4264ca141ffda942d26cdbeb9e3f60b223ce8
NIHAL KUMAR JHA <82320666+NiHaL-OO7@users.noreply.github.com>
2021-04-11 19:13:41 +0530

    Create github_nihal-oo7.js

---
+++ people/github_nihal-oo7.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'NIHAL_JHA',
+  github: 'NiHaL_OO7',
+  email: 'damon1142@gmail.com',
+  twitter: '@NiHaL_OO7',
+  facebook: 'nihal.jha.3152',
+  linkedin: 'in/nihal.jha.5797'
+};
