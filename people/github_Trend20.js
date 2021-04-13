5ade76f37b1a3ecf96947741c39c25facbaac15b
DevEnock <enockomondi305@gmail.com>
2021-04-13 16:43:38 +0300

    first contribution (#419)

---
+++ people/github_Trend20.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'ENOCK KURT',
+    github: 'Trend20',
+    email: 'enocdine305@gmail.com',
+    twitter: '@dev_enock',
+    facebook: 'Enock trend',
+    linkedin: '/in/enock-trend-273454174/'
+  };
+  
