15dba2b4d7a34115fb1dcd1247784023550076cc
R Arief <radit2707@gmail.com>
2021-04-15 08:00:59 +0700

    add add-rarief.js

---
+++ people/github_rarief.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'MrMoustache',
+  github: 'https://github.com/MrMoustache',
+  email: 'MrMoustache@gmail.com',
+  twitter: '@MrMoustache',
+  facebook: 'https://www.facebook.com/MrMoustache/',
+  linkedin: 'in/MrMoustache'
+};
