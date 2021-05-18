2da21276adf9447e938d394aed70e50cf92bcb29
tbfreitas <tarcisiodf23@gmail.com>
2021-05-18 05:55:48 -0300

    adding new user (#424)
    Co-authored-by: Tarcisio <cast.tarcisio.utic@sebrae.com.br>

---
+++ people/github_username.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Tarcísio Batista de Freitas Júnior',
+  github: 'tbfreitas',
+  email: 'tarcisiodf23@gmail.com',
+  twitter: 'N/A',
+  facebook: 'tarcisio.junior.98',
+  linkedin: 'in/tarcísio-batista-8207baa4/'
+};
