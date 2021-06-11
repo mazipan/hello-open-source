07d2acd30e4764e6aafe4967bc915df31a5b1f10
dogfood654 <69853396+dogfood654@users.noreply.github.com>
2021-06-12 05:54:14 +0800

    My first commit to open source projects (#429)

---
+++ github_kianfeng.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'kian feng',
+    github: 'dogfood654',
+    email: 'dogfood654@xxx.com',
+    twitter: '@dogfood654',
+    facebook: 'kian feng',
+    linkedin: 'in/dogfood654'
+  };
