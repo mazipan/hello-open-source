0ce9533154677e1cf8ab98a92896676dd526cca3
fakhrinail <fakhrinailwbw@gmail.com>
2021-07-30 06:31:49 +0700

    add fakhrinail.js

---
+++ people/fakhrinail.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Fakhri Nail',
+  github: 'github.com/fakhrinail',
+  email: 'fakhrinail@gmail.com',
+  twitter: '@fakhrinail',
+  facebook: 'Fakhri Nail',
+  linkedin: 'in/fakhrinail'
+};
