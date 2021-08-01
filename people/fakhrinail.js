97c0ac77ddab9a22c71b78126151f4b7fe03411e
mazipan <mazipanneh@gmail.com>
2021-08-01 14:01:05 +0000

    Purge data

--- people/fakhrinail.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Fakhri Nail',
-  github: 'github.com/fakhrinail',
-  email: 'fakhrinail@gmail.com',
-  twitter: '@fakhrinail',
-  facebook: 'Fakhri Nail',
-  linkedin: 'in/fakhrinail'
-};
