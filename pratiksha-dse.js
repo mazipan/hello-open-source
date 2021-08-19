be0e3e5d28bfff609905ab7d9c02f68010c500a6
Irfan Maulana <mazipanneh@gmail.com>
2021-08-19 07:49:18 +0700

    Delete pratiksha-dse.js

--- pratiksha-dse.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'Prateeksha',
-	github: 'pratiksha-dse',
-	email: 'palpratiksha69@gmail.com',
-	twitter: '@Pratiks17581412',
-	facebook: 'prateeksha.pal.313',
-	linkedin: 'in/prateeksha-pal-a862a41b9/'
-};
