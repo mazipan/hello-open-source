9e3fc3b668bbeebfe43295b5ea531da2e2f53a04
CFC-Mythrandir <36019346+CFC-Mythrandir@users.noreply.github.com>
2021-08-18 20:48:58 -0400

    Cfc mythrandir/add cfc mythrandir (#433)
    * git commit -m 'First commit.  Added file ~/hello-open-source/people/github_username.js'
    
    * git commit -m 'newfile'

---
+++ people/github_username.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Seamore Buts',
+  github: 'yourmom88',
+  email: 'jeffgoldblum@jenkins.com',
+  twitter: '@notyourdaddy',
+  facebook: 'who',
+  linkedin: 'in/mrpresident'
+};
