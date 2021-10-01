a548f451ac8e2b29a5d30d9e94c11a3dc1262799
Hermansyah <herman.piliang23@gmail.com>
2021-10-01 16:34:15 +0700

    chore: add-herman (#440)

---
+++ people/hermansyah32.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Hermansyah',
+    github: 'hermansyah32',
+    email: 'testmail@hermansyah.zyx',
+    twitter: 'twitwar',
+    facebook: 'facemybook',
+    linkedin: 'in/mybedroom'
+};
