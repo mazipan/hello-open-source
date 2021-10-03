2fbafc5e030f7f30028a2eeb9c9ca0a022af53cb
Okunola O Samson <68032187+pablo-clueless@users.noreply.github.com>
2021-10-03 08:16:58 +0100

    -pablo-clueless-branch (#443)

---
+++ people/pablo-clueless.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Samson Okunola',
+    github: 'pablo-clueless',
+    email: 'pablo-clueless@outlook.com',
+    twitter: '@pablo_clueless',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+  }
