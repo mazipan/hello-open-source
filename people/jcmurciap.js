e555693f4a4bea3036af10db55e37e7610e4ed6c
Juan Murcia <38795818+jcmurciap@users.noreply.github.com>
2021-10-05 23:03:32 -0500

    added a new contributor branch (#445)
    * added a new contributor branch
    
    * update name of branch

---
+++ people/jcmurciap.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Camilo',
+    github: 'jcmurciap',
+    email: 'camilo.murcia.pimentel@gmail.com',
+    twitter: '@idonothave',
+    facebook: 'idonthave',
+    linkedin: 'in/idonthave'
+}
