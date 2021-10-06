c4d6f6e0f82ed21eeaf1b47ec7d46a4259623faa
enigma <camilo.murcia.pimentel@gmail.com>
2021-10-05 20:48:59 -0500

    added a new contributor branch

---
+++ people/github_jcmurciap.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Camilo',
+    github: 'jcmurciap',
+    email: 'camilo.murcia.pimentel@gmail.com',
+    twitter: '@xxx',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+}
