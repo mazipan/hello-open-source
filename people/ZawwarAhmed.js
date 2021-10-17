b1b3330920b4bab91974f0b561812053bf7ffaec
Syed Zawwar Ahmed <64926760+SyedZawwarAhmed@users.noreply.github.com>
2021-10-17 16:22:48 +0500

    Create ZawwarAhmed.js

---
+++ people/ZawwarAhmed.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Syed Zawwar Ahmed',
+  github: 'https://github.com/SyedZawwarAhmed',
+  email: 'zawwar.ahmed12@gmail.com',
+  twitter: '@SyedZawwarAhmed',
+  facebook: 'https://www.facebook.com/zawwar.ahmed.3/',
+  linkedin: 'in/syed-zawwar-ahmed-b7345a1b8/'
+};
