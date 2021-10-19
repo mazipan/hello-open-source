a2294923e0b1b8d75743b4b55c0b7fda4b1383aa
inner-boff <75083417+inner-boff@users.noreply.github.com>
2021-10-19 04:03:50 -0300

    ADD inner-boff.js (#449)
    * ADD inner-boff.js
    
    Let's add a file. Figuring out how this works.
    
    * Update inner-boff.js
    
    * Update inner-boff.js

---
+++ people/inner-boff.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Pamelux',
+  github: 'inner-boff',
+  email: 'mmm@nnnn.com',
+  twitter: '@innerboff',
+  facebook: 'Cosme Fulanito',
+  linkedin: 'in/Pepito de los Palotes'
+}
