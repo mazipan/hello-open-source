309886696914d7986a69f9f48918063d592789ae
EinLinuus <github@linusbenkner.de>
2021-10-25 21:09:30 +0200

    Added EinLinuus

---
+++ people/EinLinuus.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Linus',
+  github: 'EinLinuus',
+  email: 'hey@linu.us',
+  twitter: '@EinLinuus',
+  facebook: 'linusbenkner',
+  linkedin: 'in/linus-benkner-257328223'
+}
