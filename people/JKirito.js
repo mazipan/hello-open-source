605310802500cfa01ba49e1d795ad67c69f86c97
JKirito <byrxarpitsingh@gmail.com>
2021-10-26 19:17:52 +0530

    Added Data to people

---
+++ people/JKirito.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Arpit Singh',
+  github: 'https://github.com/JKirito/',
+  email: 'byrxarpitsingh@gmail.com',
+  twitter: 'https://twitter.com/Jkirito7',
+  facebook: 'Arpit Singh',
+  linkedin: 'in/arpitsingh-jkirito',
+};
