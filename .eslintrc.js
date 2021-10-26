3cde304ce46d546cf144645fd13e4f42f8d526ff
John Joaquin Valdez <65848689+joaquin-wakin@users.noreply.github.com>
2021-10-26 15:44:31 +0800

    Reinitialize eslint's configuration file

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2021: true
+  },
+  extends: ['standard'],
+  parserOptions: {
+    ecmaVersion: 8
+  },
+  rules: {}
+}
