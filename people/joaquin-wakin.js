623130f753f57b6cbbf6e363e905da66e5ed8998
John Joaquin Valdez <65848689+joaquin-wakin@users.noreply.github.com>
2021-10-26 15:50:28 +0800

    Create joaquin-wakin.js

---
+++ people/joaquin-wakin.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Joaquin',
+  github: 'joaquin-wakin',
+  email: 'valdez.johnjoaquin.13579@gmail.com',
+  twitter: '@wakin_joaquin',
+  facebook: 'Joaquin Valdez',
+  linkedin: 'in/joaquin-valdez'
+}
