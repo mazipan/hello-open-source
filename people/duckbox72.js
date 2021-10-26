3c4d40c409bf4ad76527fdac709effeaf0c603af
Luis Felipe Klaus <duckbox@hotmail.com>
2021-10-26 08:58:48 -0300

    add-duckbox72

---
+++ people/duckbox72.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Luis Felipe Klaus',
+    github: 'duckbox72',
+    email: 'duckbox72@gmail.com',
+    twitter: '@falecomopato',
+    facebook: 'Luis Felipe Klaus',
+    linkedin: 'in/xxx'
+  };
