01b28a09e0967eedb29d580dc7077d05f169d26d
Usama Bagwan <71325129+iusamahub@users.noreply.github.com>
2021-11-04 17:40:15 +0530

    Create github_iusamahub.js

---
+++ people/github_iusamahub.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Usama Bagwan',
+  github: 'https://github.com/iusamahub',
+  email: 'usamaas100@gmail.com',
+  twitter: 'iusama10',
+  facebook: 'xxx',
+  linkedin: 'https://www.linkedin.com/in/usama-bagwan/'
+};
