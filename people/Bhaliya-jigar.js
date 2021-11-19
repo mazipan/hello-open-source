8d28837de53b14fdf4c59deb5e960a2082982ed6
Bhaliya-jigar <jigarbhaliya@gmail.com>
2021-11-19 15:15:41 +0530

    Adding Bhaliya-jigar to people list

---
+++ people/Bhaliya-jigar.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Bhaliya Jigar',
+    github: 'Bhaliya-jigar',
+    email: 'jigarbhaliya@.com',
+    twitter: '@xxx',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+  };
