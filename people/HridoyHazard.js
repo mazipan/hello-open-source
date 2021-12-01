8ba88dd1dbb28e269e94a46183c083d9f2af5841
mazipan <mazipanneh@gmail.com>
2021-12-01 14:01:44 +0000

    Purge data

--- people/HridoyHazard.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'HridoyHazard',
-  github: 'HridoyHazard',
-  email: 'hridoyhazard47@gmail.com',
-  twitter: '-',
-  facebook: '-',
-  linkedin: '-'
-};
--- people/courtneyzhan.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Connie Zhang',
-  github: 'courtneyzhan',
-  email: 'veronicafields@outlook.com',
-  twitter: '@courtneyzhan',
-  facebook: 'None',
-  linkedin: 'None'
-}
