0eb33ab0361b8780683826f46c874ecdca986cd3
renanortiz <renan.ortiz@live.com>
2021-12-10 12:49:10 -0300

    adding info2

--- people/renanortiz.js
+++ people/renanortiz.js
@@ -1,9 +1,9 @@
 module.exports = {
-    name: 'Renan',
-    github: 'renanortiz',
-    email: 'renan.ortiz@live.com',
-    twitter: '@xxx',
-    facebook: 'xxx',
-    linkedin: 'in/xxx'
+  name: 'Renan',
+  github: 'renanortiz',
+  email: 'renan.ortiz@live.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/xxx'
   }
   
