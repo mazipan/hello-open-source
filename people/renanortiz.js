7f593ef2726f723987bbd5fc5f47502218423025
renanortiz <renan.ortiz@live.com>
2021-12-10 12:25:20 -0300

    adding my name

--- people/renanortiz.js
+++ people/renanortiz.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'Renan Ortiz',
     github: 'renanortiz',
     email: 'renan.ortiz@live.com',
-    twitter: '@xxx',
-    facebook: 'xxx',
-    linkedin: 'in/xxx'
-  };
+    twitter: '/test',
+    facebook: '/test2',
+    linkedin: 'in/test3'
+}
