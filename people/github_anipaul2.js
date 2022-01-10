edc9e27ff854dc3f0413f251d7b26435a2e8e846
anipaul2 <aniketh131@gmail.com>
2022-01-10 20:07:07 +0530

    error fixed of my previous PR

--- people/github_anipaul2.js
+++ people/github_anipaul2.js
@@ -1,8 +1,9 @@
 module.exports = {
-    name: 'Aniketh Paul',
-    github: 'https://github.com/anipaul2',
-    email: 'aniketh131@gmail.com',
-    twitter: 'https://twitter.com/aniketh_88',
-    facebook: 'Sorry, I dont use Facebook.',
-    linkedin: 'https://www.linkedin.com/in/aniketh-paul-7728691bb/'
-  };
+  name: 'Aniketh Paul',
+  github: 'https://github.com/anipaul2',
+  email: 'aniketh131@gmail.com',
+  twitter: 'https://twitter.com/aniketh_88',
+  facebook: 'None',
+  linkedin: 'https://www.linkedin.com/in/aniketh-paul-7728691bb/'
+};
+
