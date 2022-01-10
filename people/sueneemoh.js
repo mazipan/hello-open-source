8e52dff08a4f3e3d050287dfcf59230c1e18db0a
anipaul2 <aniketh131@gmail.com>
2022-01-10 20:38:25 +0530

    Fixed error of two more people

--- people/sueneemoh.js
+++ people/sueneemoh.js
@@ -1,8 +1,8 @@
 module.exports = {
-    name: 'YOUR_NAME',
-    github: 'XXX',
-    email: 'xxx@xxx.com',
-    twitter: '@xxx',
-    facebook: 'xxx',
-    linkedin: 'in/xxx'
-  };
+  name: 'YOUR_NAME',
+  github: 'XXX',
+  email: 'xxx@xxx.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/xxx'
+}
