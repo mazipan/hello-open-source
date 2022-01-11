1949030bd53345f90520a232286f224913f4bcba
Aniketh Paul <aniketh131@gmail.com>
2022-01-11 07:03:33 +0530

    Add me to the people file (#480)
    * Add me to the people file
    
    * error fixed of my previous PR
    
    * Fixed two more errors in my previous PR's
    
    * Added a newline
    
    * Fixed error of two more people

---
+++ people/github_anipaul2.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Aniketh Paul',
+  github: 'https://github.com/anipaul2',
+  email: 'aniketh131@gmail.com',
+  twitter: 'https://twitter.com/aniketh_88',
+  facebook: 'None',
+  linkedin: 'https://www.linkedin.com/in/aniketh-paul-7728691bb/'
+}
