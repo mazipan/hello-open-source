7a636f9c6749d9b508982c340f3210358218c8a7
zmrhussain <zameerhussain8531@gmail.com>
2022-01-13 13:26:38 +0530

    Add me to the people file

---
+++ people/zmrhussain.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'zameer hussain',
+  github: 'zmrhussain',
+  email: 'xxx@xxx.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/zameer-hussain-b03575175/'
+};
