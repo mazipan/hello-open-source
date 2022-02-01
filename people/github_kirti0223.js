891abbb2070619e5eff0fb17ffed76f2b3869b84
kirti0223 <kirtisingla58@gmail.com>
2022-02-01 10:58:43 +0530

    Kirti added

--- people/github_kirti0223.js
+++ people/github_kirti0223.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'Aniketh Paul',
+    name: 'Kirti Singla',
     github: 'https://github.com/kirti0223',
     email: 'xyz@gmail.com',
     twitter: 'https://twitter.com/xyz',
