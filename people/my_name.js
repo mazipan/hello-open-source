a1b3d3bc20851b3f02ed5f8e9d80f8b481123246
Chigozie Oduah <87097037+TheGhoulRe@users.noreply.github.com>
2022-03-26 21:05:52 +0100

    my-contribution

---
+++ people/my_name.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Someone Unknown',
+    github: 'TheGhoulRe',
+    email: 'showmetheway@this.town',
+    twitter: 'GhoulKingR',
+    facebook: 'Iveforgotten',
+    linkedin: 'https://www.linkedin.com/in/chigozie-oduah-b985b5214/'
+};
