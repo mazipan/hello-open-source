4aae7b27620ac4a1f19b742b1b0b9427a02ece6e
agm_was_here <aswinmenon92@gmail.com>
2022-04-11 22:02:32 +0530

    Create github_agm-was-here.js

---
+++ people/github_agm-was-here.js
@@ -0,0 +1,8 @@
+module.exports ={
+ name: 'Aswin G Menon',
+  github: 'https://github.com/agm-was-here',
+  email: 'agmwashere@gmail.com',
+  twitter: 'https://twitter.com/agm_was_here',
+  facebook: 'None',
+  linkedin: 'https://www.linkedin.com/in/agm-was-here/'
+}
