22b4057eb758d967b676d18a65319e4ac3814678
Avinash Prajapati <76595361+avinashprj@users.noreply.github.com>
2022-06-11 23:22:54 +0530

    Create avinashprj.js (#492)
    Added my information in the people folder

---
+++ people/avinashprj.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Avinash prajapati',
+  github: 'avinashprj',
+  email: 'avinashprajapati914@gmail.com',
+  twitter: '@avinash_prj',
+  facebook: 'N.A',
+  linkedin: 'in/avinash-prajapati'
+};
