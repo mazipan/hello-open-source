d1d1b797c6b0d9edecae4817fe8910a3dd3d0db1
Irfan Maulana <mazipanneh@gmail.com>
2022-06-12 00:54:08 +0700

    Delete peoples directory

--- peoples/gatomalandro.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'Lawliet',
-    github: 'gatomalandro',
-    email: 'randomdata@blabla.com',
-    twitter: '@nopnop',
-    facebook: 'hahaha',
-    linkedin: 'in/whatever'
-  }
