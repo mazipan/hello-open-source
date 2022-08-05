19c3abce7d6003138739c4f7890f2c057f48310c
sukriti sinha <98272003+Sukriti8877@users.noreply.github.com>
2022-08-05 16:39:06 +0530

    Add files via upload
    Added Sukriti8877.js

---
+++ people/Sukriti8877.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Sukriti Sinha',
+    github: 'Sukriti8877',
+    email: 'sukritisinha654@gmail.com',
+    twitter: '@SukritiSinha10',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+};
