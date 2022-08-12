432535c6d10c383787dcbe7e06eea30463ab38bb
Aryan-Jagadale <aryansjagadale@gmail.com>
2022-08-12 15:52:16 +0530

    Started Open source

---
+++ people/Aryan-Jagadale.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Aryan Jagadale',
+    github: 'https://github.com/Aryan-Jagadale',
+    email: 'xxx',
+    twitter: '@xxx',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+  };
