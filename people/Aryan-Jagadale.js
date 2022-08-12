ad0423b5e3cd2f962b11b60b4d60fb79005446b4
Aryan-Jagadale <aryansjagadale@gmail.com>
2022-08-12 15:56:49 +0530

    Solving ESlint error

--- people/Aryan-Jagadale.js
+++ people/Aryan-Jagadale.js
@@ -1,8 +1,1 @@
-module.exports = {
-    name: 'Aryan Jagadale',
-    github: 'https://github.com/Aryan-Jagadale',
-    email: 'xxx',
-    twitter: '@xxx',
-    facebook: 'xxx',
-    linkedin: 'in/xxx'
-  };
+module.exports = {name: 'Aryan Jagadale',github: 'https://github.com/Aryan-Jagadale',email: 'xxx',twitter: '@xxx',facebook: 'xxx',linkedin: 'in/xxx'}
