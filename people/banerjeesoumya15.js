5e8f655d58a5e5a946b67b039720f2d2a3aef283
Soumya Shankar Banerjee <banerjeesoumya15@gmail.com>
2022-08-30 00:05:40 +0530

    Create banerjeesoumya15.js

---
+++ people/banerjeesoumya15.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Soumya',
+  github: 'banerjeesoumya15',
+  email: 'banerjeesoumya15@gmail.com',
+  twitter: '@soumya_data',
+  linkedin: 'in/soumya-shankar-banerjee/'
+};
