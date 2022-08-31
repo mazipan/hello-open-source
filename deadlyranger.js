e75bb8f7066a9190a9f07e2dc3071902e99625a6
Mudit Kubrey <94749969+DeadlyRanger@users.noreply.github.com>
2022-08-31 19:28:05 +0530

    Create deadlyranger.js

---
+++ deadlyranger.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Mudit kubrey',
+  github: 'deadlyranger',
+  email: 'padaikrle13@gmail.com',
+  twitter: '@onesideloverz',
+  facebook: 'xxx',
+  linkedin: 'htttps://www.linkedin.com/in/mudit-kubrey'
+};
