be12bdc963a27a756c5ffc33004b8733569cce42
Irfan Maulana <mazipanneh@gmail.com>
2022-09-05 12:52:30 +0700

    Delete deadlyranger.js

--- deadlyranger.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Mudit kubrey',
-  github: 'deadlyranger',
-  email: 'padaikrle13@gmail.com',
-  twitter: '@onesideloverz',
-  facebook: 'xxx',
-  linkedin: 'htttps://www.linkedin.com/in/mudit-kubrey'
-};
