bc147e82430fd9f66bea1b91fe843b0f6f02e852
TenTraicion <103281314+TenTraicion@users.noreply.github.com>
2022-09-19 09:29:35 +0600

    Create TenTraicion.js (#519)
    Add my information ins TenTraicion.js

---
+++ people/TenTraicion.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'TenTraicion',
+  github: 'TenTraicion',
+  email: 'TenTraicion@gmail.com',
+  twitter: '@TenTraicion',
+  facebook: 'TenTraicion',
+  linkedin: 'in/TenTraicion'
+}
