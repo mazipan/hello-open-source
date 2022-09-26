7e0f4038f06185ea5602932bd9c9047fa83bc01f
Nayana Pardhekar <nayanapardhekar@gmail.com>
2022-09-26 19:48:08 +0530

    Create nayanapardhekar.js
    Created a js file to contribute to this project.

---
+++ people/nayanapardhekar.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Nayana Pardhekar',
+  github: 'https://github.com/nayanapardhekar',
+  email: 'xxx@xxx.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'https://www.linkedin.com/in/nayana-pardhekar-24a832204/'
+}
