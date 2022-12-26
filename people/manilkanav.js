a53be2fb3c2404561d49db5820528b61af23500d
Manilka Navod <manilka.projects@gmail.com>
2022-12-26 12:03:50 +0530

    Add manilkanav.js

---
+++ people/manilkanav.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'MANILKA NAVOD DIKKUMBURA',
+    github: 'manilkanav',
+    email: 'manilkadikkumbura17@gmail.com',
+    facebook: 'Manilka Dikkumbura',
+    linkedin: 'in/manilka-dikkumbura-409b70248'
+  }
