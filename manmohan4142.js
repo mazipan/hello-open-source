ce51d11d8a74d2c015409746fd62a1733be328b7
Irfan Maulana <mazipanneh@gmail.com>
2022-12-27 14:47:17 +0700

    Delete manmohan4142.js

--- manmohan4142.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Manmohan Dhal',
-  github: 'manmohan4142',
-  email: 'rk1619606@gmail.com',
-  twitter: 'https://twitter.com/home',
-  facebook: 'https://www.facebook.com/',
-  linkedin: 'https://in.linkedin.com/'
-}
