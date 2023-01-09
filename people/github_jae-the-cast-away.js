6b44147e1d8bbc5b166ff8bd16eba1f721d59a0f
Jaeheon Kim <jaeheonkim95@gmail.com>
2023-01-09 17:03:15 -0600

    first commit

---
+++ people/github_jae-the-cast-away.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'jae-the-castaway',
+    github: 'jae-the-castaway',
+    email: 'jaeheonkim95@gmail.com',
+    twitter: '@xxx',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+  }
