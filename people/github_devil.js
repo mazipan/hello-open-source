557adc194fe0b79767cfab5e1675e1667cdca089
D3vilOPエース <97296994+DevilexeOP@users.noreply.github.com>
2023-02-06 21:34:54 +0530

    added devil

---
+++ people/github_devil.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Usama Bagwan',
+    github: 'https://github.com/DevilexeOP',
+    email: 'devilgfx169@gmail.com',
+    twitter: 'N/A',
+    facebook: 'N/A',
+    linkedin: 'N/A'
+  };
