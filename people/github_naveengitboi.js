b21fb94ca15f4b975057ccd4425298e6ede80e3a
NaveenCodes <110653456+naveengitboi@users.noreply.github.com>
2023-02-23 13:27:27 +0530

    added username naveengitboi (#539)

---
+++ people/github_naveengitboi.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'naveengitboi',
+    github: 'https://github.com/naveengitboi',
+    email: 'abc@gmial.com',
+    twitter: '@navesays',
+    facebook: '@facebook',
+    linkedin: 'in/@linkedin'
+}
