e20da1c779d27f180337dd6c5b03df70d69fc3b0
Milan Sachani <79278557+Milan-960@users.noreply.github.com>
2023-03-14 06:37:45 +0100

    user name added (#544)

---
+++ people/github_milan.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Milan',
+    github: 'https://github.com/milan-960',
+    email: 'milan@gmail.com',
+    twitter: '@milansachani',
+    facebook: '',
+    linkedin: 'in/@linkedin'
+}
