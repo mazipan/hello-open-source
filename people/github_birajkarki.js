43bc06f6fc163c622060f00eec60e7eecd07d52e
Biraj Karki <birajkarki9849@gmail.com>
2023-04-13 09:57:01 +0545

    hello open source (#545)

---
+++ people/github_birajkarki.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Biraj Karki',
+    github: 'https://github.com/birajkarki',
+    email: 'birajkarki9849@gmail.com',
+    twitter: '@thelifeofbiraj',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+}
