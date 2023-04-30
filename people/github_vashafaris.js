aae4d406fc36aea035976726669e35588691f2c9
Vasha Farisi <55099418+vashafaris@users.noreply.github.com>
2023-04-30 08:57:24 +0700

    added vashafaris (#549)

---
+++ people/github_vashafaris.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Vasha Farisi',
+  github: 'https://github.com/vashafaris',
+  email: 'N/A',
+  twitter: '@vashafarisi',
+  facebook: 'N/A',
+  linkedin: 'https://www.linkedin.com/in/vashafarisi/'
+}
