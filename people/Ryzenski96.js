6d22ff00a19de4d7df1f4815bbce26d6d0486091
mazipan <mazipanneh@gmail.com>
2023-05-01 14:01:03 +0000

    🔥 CHORE: purge all data

--- people/Ryzenski96.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'Ryan Wisniewski',
-    github: 'Ryzenski96',
-    email: 'ryan.wisniewski1996@gmail.com',
-    twitter: '@xxx',
-    facebook: 'xxx',
-    linkedin: 'in/xxx',
-}
