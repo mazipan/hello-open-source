b08a7ed9dcf1d41f771bcdc131e3fd6a7274186c
Ryan <ryan.wisniewski1996@gmail.com>
2023-05-01 07:19:22 -0400

    Create Ryzenski96.js (#551)

---
+++ people/Ryzenski96.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Ryan Wisniewski',
+    github: 'Ryzenski96',
+    email: 'ryan.wisniewski1996@gmail.com',
+    twitter: '@xxx',
+    facebook: 'xxx',
+    linkedin: 'in/xxx',
+}
