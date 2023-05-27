688d6d58a188a43d74b6c28def51bcc770f13cb5
Ganivada Mouli <vishnumouli0@gmail.com>
2023-05-27 08:44:39 +0530

    Added my Information

---
+++ people/vishnu-mouli-102408.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Vishnu_Mouli',
+  github: 'https://github.com/vishnu-mouli-102408',
+  email: 'vishnumouli0@gmail.com',
+  twitter: '@iamVishnuMouli',
+  linkedin: 'in/ganivada-mouli'
+}
