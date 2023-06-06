1c1004e0c1ede6216f73e7012aa9672bd7775d62
Shubham Yeram <127608511+0x1Shub@users.noreply.github.com>
2023-06-06 10:23:34 +0530

    first contribution (#558)
    Co-authored-by: shubham <shubhamyeram13@gmail.com>

---
+++ people/github_0x1Shub.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Shubham',
+  github: 'https://github.com/0x1Shub',
+  email: 'shubhamyeram13@gmail.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/xxx'
+}
