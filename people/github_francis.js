e0ac681c7c37ac194c1cf66ad5c7c7364e30db5d
Brefofrancis <72323354+Brefofrancis@users.noreply.github.com>
2023-06-06 04:53:50 +0000

    Added my file to the project (#557)

---
+++ people/github_francis.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Francis',
+  github: 'https://github.com/Brefofrancis',
+  email: 'brefofrancisasamaoh.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/xxx'
+}
