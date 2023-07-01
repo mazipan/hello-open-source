56ef6dfabcf7c568aa13324434c019bc49b4293e
mazipan <mazipanneh@gmail.com>
2023-07-01 14:00:51 +0000

    🔥 CHORE: purge all data

--- people/github_0x1Shub.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Shubham',
-  github: 'https://github.com/0x1Shub',
-  email: 'shubhamyeram13@gmail.com',
-  twitter: '@xxx',
-  facebook: 'xxx',
-  linkedin: 'in/xxx'
-}
--- people/github_francis.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Francis',
-  github: 'https://github.com/Brefofrancis',
-  email: 'brefofrancisasamaoh.com',
-  twitter: '@xxx',
-  facebook: 'xxx',
-  linkedin: 'in/xxx'
-}
