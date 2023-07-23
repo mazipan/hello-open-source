0727b0029a757d1ff6b74a21dbc530df2cadf161
Andre Alves <51288376+arenodi@users.noreply.github.com>
2023-07-22 21:57:41 -0300

    Adding 'arenodi.js' file to 'people' folder (#563)

---
+++ people/arenodi.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'hereweare',
+    github: 'learningstuff',
+    email: 'text@text.com',
+    twitter: '@bluebird',
+    facebook: 'caralivro',
+    linkedin: 'in/linkado'
+  }
