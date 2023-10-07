11918482cb597021e1aaaee4d7dead3d39d31467
Nishant Pandey <108941493+Nishan9t@users.noreply.github.com>
2023-10-07 12:08:55 +0530

    added Nisha9t (#567)

---
+++ people/Nishan9t.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'nishant pandey',
+    github: 'Nishan9t',
+    email: 'pandeynisha9t@gmail.com',
+    twitter: '@nis.9t',
+    facebook: 'nishant pandey',
+    linkedin: 'in/Nishant-pandey'
+  }
