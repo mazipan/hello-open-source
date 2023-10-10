229557ece9b44f0f0df90c11a886deb40124e563
bhargavshirin <bnalamat@gitam.in>
2023-10-10 20:27:42 +0530

    added my file

---
+++ people/bharg.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Bhargav Shirin',
+    github: 'bhargavshirin',
+    email: 'bhargav1@gmail.com',
+    twitter: '@bhar11',
+    facebook: 'bhargav',
+    linkedin: 'in/bhargavshirin'
+  }
