0294b909c22e678016e3046bdbffc681f0902bae
Sanyam Jain <sanyamjain6667@gmail.com>
2023-10-12 00:45:16 +0530

    Create Sanyam-2026.js (#572)

---
+++ people/Sanyam-2026.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Sanyam Jain',
+  github: 'https://github.com/Sanyam-2026',
+  email: 'sanyamjain6667@gmail.com',
+  twitter: '@IasSanyamJain',
+  facebook: 'https://www.facebook.com/sanyam.jain.31392',
+  linkedin: 'in/sanyam-jain-874892201/'
+}
