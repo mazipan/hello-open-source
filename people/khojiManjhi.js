9ecfdd226994e9214b72f807faf70963ca42dde2
Ash V <96707067+tvermaashutosh@users.noreply.github.com>
2023-10-13 01:25:55 +0530

    Created khojiManjhi.js

---
+++ people/khojiManjhi.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Khoji Manjhi',
+  github: 'khojiManjhi',
+  email: 'khojiManjhi@com.com',
+  twitter: '@khojiManjhi',
+  facebook: 'khojiManjhi',
+  linkedin: 'in/khojiManjhi'
+}
