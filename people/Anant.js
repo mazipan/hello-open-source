c6829d31cc39f0fa4d8cc7e6bde455832c4631e3
Abhishekgupta204 <116148980+Abhishekgupta204@users.noreply.github.com>
2023-10-13 00:59:12 +0530

    Create Anant.js

---
+++ people/Anant.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'Anant Jain',
+    github: 'https://github.com/Anant',
+    email: 'anantjain7876@gmail.com',
+    twitter: '@AnantJain',
+    facebook: 'https://www.facebook.com/anant.jain.31584',
+    linkedin: 'in/anant-jain-5847961557/'
+  }
+  
