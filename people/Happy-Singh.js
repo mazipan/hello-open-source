c9bccba4f8dfa30c680c604dc469ba6d4be89767
Arcturus22 <99889376+Arcturus22@users.noreply.github.com>
2023-10-13 02:31:30 +0530

    Created and Updated Happy-Singh.js

---
+++ people/Happy-Singh.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Happy Signh',
+  github: 'https://github/p/HappySingh',
+  email: 'happy@singh123.com',
+  twitter: '@noob',
+  facebook: 'https://www.facebook.com/noobBOY',
+  linkedin: 'in/happy-singh/noob'
+}
