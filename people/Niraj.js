1e8c4733b1076025f675ac7323a2506c6ce3b145
Niraj <128296263+btrezzy@users.noreply.github.com>
2023-10-14 12:11:21 +0530

    Added my profile

---
+++ people/Niraj.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Nirajkumar',
+  github: 'https://github.com/btrezzy',
+  email: 'nirajkumarpatel21@gmail.com',
+  twitter: '@btreezy',
+  facebook: 'https://www.facebook.com/nirajpatel',
+  linkedin: 'https://www.linkedin.com/in/niraj-patel-a23987247/'
+}
