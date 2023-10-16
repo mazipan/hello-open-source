cb0b1d203bc2d782d6405ab2b8164e700f7edab8
amandeep singh parihar <amandeepparihar24@gmail.com>
2023-10-16 23:40:22 +0530

    done

---
+++ people/amandeep-singh-parihar.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'amandeep-singh-parihar',
+    github: 'https://github.com/amandeep-singh-parihar',
+    email: 'pisn604@gmail.com',
+    twitter: '@poison375996',
+    facebook: 'xxx',
+    linkedin: 'https://www.linkedin.com/in/deep-parihar-8399aa25a/'
+  }
