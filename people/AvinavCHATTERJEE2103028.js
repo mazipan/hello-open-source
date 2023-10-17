dd5a05afebcba3a81afd1b85b015e740475e03f6
Subhayu Dutta <96583409+AvinavCHATTERJEE2103028@users.noreply.github.com>
2023-10-17 18:35:29 +0530

    addedjsfile

---
+++ people/AvinavCHATTERJEE2103028.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Avinav Chatterjee',
+    github: 'https://github.com/AvinavCHATTERJEE2103028',
+    email: 'avinavchatterjee63@gmail.com',
+    twitter: '@AvinavCHATTERJEE2103028',
+    facebook: 'AvinavCHATTERJEE2103028',
+    linkedin: 'in/AvinavCHATTERJEE2103028'
+  }
