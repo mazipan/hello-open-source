c33fe198f37aaa21ceb6aa24e236818ddba9ef32
0x00zer0day <0x00zer0day@gmail.com>
2023-10-17 14:55:44 +0530

    Added User Info

---
+++ people/0x00zer0day.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'WAHID SHAIKH',
+  github: 'https://github.com/0x00zer0day',
+  email: '0xzer0day@proton.me',
+  twitter: '@0xfsociety00',
+  facebook: 'null',
+  linkedin: 'null'
+}
