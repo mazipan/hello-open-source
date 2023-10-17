3ce7dec9b20cf493ba15e25f1a2f5d0b233209d3
amitverma147 <vamit20000@gmail.com>
2023-10-17 10:20:36 +0530

    updated

---
+++ people/amitverma.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Amit Verma',
+    github: 'https://github.com/amitverma147',
+    email: 'kushamit147@gmail.com',
+    twitter: '@Amit_v6',
+    // facebook: 'xxx',
+    linkedin: 'in/amit-verma-883150109'
+  }
