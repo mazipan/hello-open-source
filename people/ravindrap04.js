373ddedb57a7053408a56ffa7cb96b4ab44f1e16
Ravindra Pandey <98400348+RavindraP04@users.noreply.github.com>
2023-10-17 08:14:01 +0000

    add-ravindrap04

---
+++ people/ravindrap04.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'Ravindra Pandey',
+    github: 'https://github.com/RavindraP04',
+    email: 'ravindrapandey99999@gmail.com',
+    twitter: '@ravindrap04',
+    linkedin: 'in/ravindrap04'
+  }
