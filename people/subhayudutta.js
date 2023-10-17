e7c76c06194753e1c731ac5420105fe3629a6d10
Subhayu Dutta <97235543+subhayudutta@users.noreply.github.com>
2023-10-17 16:11:52 +0530

    ProfileAdded (#602)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ people/subhayudutta.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Subhayu Dutta',
+    github: 'https://github.com/subhayudutta',
+    email: 'subhayudutta2@gmail.com',
+    twitter: '@subhayudutta0',
+    facebook: 'subhayudutta',
+    linkedin: 'in/subhayudutta'
+  }
