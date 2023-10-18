a0db33524b19d92c780d2a314f349ebcd775acc3
Muhammad Adjie Bimaditya <madjiebimaa@gmail.com>
2023-10-18 13:05:38 +0700

    Add madjiebimaa.js file (#605)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ people/madjiebimaa.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'Muhammad Adjie Bimaditya',
+    github: 'https://github.com/madjiebimaa',
+    email: 'madjiebimaa@gmail.com',
+    twitter: '@madjiebimaa',
+    linkedin: 'in/madjiebimaa'
+  }
