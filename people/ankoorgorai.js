eafa59093395db8d002378d1dfae5105ea76e8f3
ankoorgorai <148214358+ankoorgorai@users.noreply.github.com>
2023-10-18 11:36:24 +0530

    profileAdded (#603)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

---
+++ people/ankoorgorai.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Ankoor Gorai',
+    github: 'https://github.com/ankoorgorai',
+    email: 'ankoorgorai@gmail.com',
+    twitter: '@ankoorgorai',
+    facebook: 'ankoorgorai',
+    linkedin: 'in/ankoorgorai'
+  }
