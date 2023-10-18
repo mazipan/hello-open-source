41ff89445048d33bc15d1b1f9edb31abf1449e54
y4sh1k4 <130989092+y4sh1k4@users.noreply.github.com>
2023-10-18 16:12:55 +0530

    Create y4sh1k4.js

---
+++ people/y4sh1k4.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Yashika',
+  github: 'https://github.com/y4sh1k4/',
+  email: 'mehndirattayashika5@gmail.com',
+  twitter: 'xxx',
+  facebook: 'xxx',
+  linkedin: 'https://www.linkedin.com/in/yashika-mehndiratta-694268248/'
+}
