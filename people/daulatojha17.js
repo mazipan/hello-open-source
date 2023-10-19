79bdcc4d727e2eb501792672e8e4f060e6950a0a
mazipan <mazipanneh@gmail.com>
2023-10-19 10:21:20 +0000

    🔥 CHORE: purge all data

--- people/daulatojha17.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Daulat Ojha',
-  github: 'https://github.com/daulatojha17',
-  email: 'daulatojha17@gmail.com',
-  twitter: '@daulatojha',
-  facebook: '-',
-  linkedin: 'https://www.linkedin.com/in/daulat-ojha-75497a228/'
-}
