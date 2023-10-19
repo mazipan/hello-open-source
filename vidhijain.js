b3678673f1cd15d708342651cd594ed517748ea1
daulatojha17 <117494705+daulatojha17@users.noreply.github.com>
2023-10-19 10:59:00 +0530

    Update vidhijain.js

--- vidhijain.js
+++ vidhijain.js
@@ -7,37 +7,36 @@
 </head>  
 <body>  
     <h3>Choose the one you like</h3>  
-    <input type="radio" id="happy" name="status">Be Happy with what you have</br>  
-    <input type="radio" id="excited" name="status">Be Excited about what you want</br>  
-    <input type="radio" id="sad" name="status">Don't be Sad </br>  
-    <input type="radio" id="angry" name="status">Anger is your biggest enemy</br>  
-    <input type="radio" id="none" name="status">That's great</br>  
-<p id="show"></p>  
+    <input type="radio" id="happy" name="status">Be Happy with what you have<br> <!-- "br" tag corrected -->
+    <input type="radio" id="excited" name="status">Be Excited about what you want<br> <!-- "br" tag corrected -->
+    <input type="radio" id="sad" name="status">Don't be Sad <br> <!-- "br" tag corrected -->
+    <input type="radio" id="angry" name="status">Anger is your biggest enemy<br>
+    <input type="radio" id="none" name="status">That's great<br>
+    <p id="show"></p>  
     <script>  
         let show = document.querySelector('#show');  
         document.body.addEventListener('change', function (e) {  
             let target = e.target;  
             let display_msg;  
             switch (target.id) {  
-           case 'happy':  
+                case 'happy':  
                     display_msg = 'You are happy';  
                     break;  
-            case 'excited':  
+                case 'excited':  
                     display_msg = 'You are excited';  
                     break;  
-             case 'sad':  
-                   display_msg = 'You are sad';  
-                   break;  
-    case 'angry':  
-                   display_msg = 'You are angry';  
-                   break;  
-    case 'none':  
-                   display_msg = 'none';  
-                   break;  
+                case 'sad':  
+                    display_msg = 'You are sad';  
+                    break;  
+                case 'angry':  
+                    display_msg = 'You are angry';  
+                    break;  
+                case 'none':  
+                    display_msg = 'That\'s great'; // Corrected text for 'none'
+                    break;  
             }  
             show.textContent = display_msg;  
         });
-yarn run lint --fix
     </script>  
 </body>  
-</html>  
+</html>
