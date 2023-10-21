b9834db5e82ef072c7bf396a016f6e2c688dc407
Alisha Parveen <139856223+Alisha-786@users.noreply.github.com>
2023-10-22 00:34:51 +0530

    Create Alisha.js

---
+++ people/Alisha.js
@@ -0,0 +1,6 @@
+module.exports = {
+    name: 'Alisha',
+    github: 'https://github.com/Alisha-786',
+    linkedin: 'https://www.linkedin.com/in/alisha-parveen-a7a718286/',
+    twitter: 'https://twitter.com/alisha_7865'
+  }
