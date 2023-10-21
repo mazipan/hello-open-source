544919e479051ae3d0c1bf0d1112853d93b84b87
Upendra <96371563+Upendra2003@users.noreply.github.com>
2023-10-21 11:53:28 +0530

    Create Upendra.js

---
+++ people/Upendra.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Upendra',
+    github: 'Upendra2003',
+    email: 'upendrakarimi2003@gmail.com',
+    twitter: '@upendra',
+    facebook: 'upendra',
+    linkedin: 'in/karimi-upendra'
+  }
