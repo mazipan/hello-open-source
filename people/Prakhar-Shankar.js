0e1fe9e2d55e0470c3bc1b1e7c6c538afe34973d
Prexy <97254881+Prakhar-Shankar@users.noreply.github.com>
2023-10-21 15:40:28 +0530

    Create Prakhar-Shankar.js

---
+++ people/Prakhar-Shankar.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Prakhar_Shankar',
+  github: 'https://github.com/Prakhar-Shankar',
+  email: 'prakharshankar247@gmail.com',
+  twitter: 'xxx',
+  facebook: 'xxx',
+  linkedin: 'https://www.linkedin.com/in/prakhar-shankar-24a788227/'
+}
