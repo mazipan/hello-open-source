e398f21be512588d696f42ecb79407f6165afa3a
Sai Rohan Reddy Bobitti <123877564+RohanReddy2003@users.noreply.github.com>
2023-10-21 12:41:51 +0530

    Create Rohan.js

---
+++ people/Rohan.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'rohan',
+  github: 'https://github.com/RohanReddy2003',
+  email: 'bobbitisairohan@gmail.com',
+  twitter: 'https://twitter.com/sairohanreddy',
+  facebook: 'https://facebook.com/sairohanreddy',
+  linkedin: 'https://www.linkedin.com/in/rohanreddy'
+}
