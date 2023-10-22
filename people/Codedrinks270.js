f602e8931c1de8a0bcc30e1345532f68ac885345
CodeDrinks <144559020+CodeDrinks270@users.noreply.github.com>
2023-10-22 17:21:12 +0530

    Added Codedrinks270.js

---
+++ people/Codedrinks270.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Code Drinks',
+  github: 'https://github.com/CodeDrinks270',
+  email: 'codedrinks270@gmail.com',
+  twitter: 'https://twitter.com/Khushminde14681',
+  facebook: 'https://www.facebook.com/profile.php?id=100077569230303',
+  linkedin: 'https://www.linkedin.com/in/khushminder-singh-95b66074/'
+}
