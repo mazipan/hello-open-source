0841f4eb768ddb37796e32442ebea860d797db43
rahulvp105 <81913202+rahulvp105@users.noreply.github.com>
2023-10-22 20:35:46 +0530

    Update rahulvp105.js

--- people/rahulvp105.js
+++ people/rahulvp105.js
@@ -2,8 +2,6 @@ module.exports = {
     name: 'Rahul',
     github: 'https://github.com/rahulvp105',
     email: 'rahulprajapati2524@gmail.com',
-    twitter: 'https://twitter.com/',
-    facebook: '',
-    linkedin: 'https://www.linkedin.com/in/rahul/'
+    
   }
   
