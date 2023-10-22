288f74edaf19ce81c6ad8001dd4e95f1afd2de68
rahulvp105 <81913202+rahulvp105@users.noreply.github.com>
2023-10-22 20:03:15 +0530

    Create rahulvp105.js

---
+++ people/rahulvp105.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'Rahul',
+    github: 'https://github.com/rahulvp105',
+    email: 'rahulprajapati2524@gmail.com',
+    twitter: 'https://twitter.com/',
+    facebook: '',
+    linkedin: 'https://www.linkedin.com/in/rahul/'
+  }
+  
