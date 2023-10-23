7addaa6070eb794d2b6ff9af7bef02089b50b449
Sudhanshu Tripathi <97780892+sudhanshu-77@users.noreply.github.com>
2023-10-23 23:57:58 +0530

    add-sudhanshu-77.js

---
+++ people/add-sudhanshu-77.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Sudhanshu Tripathi',
+  github: 'https://github.com/sudhanshu-77',
+  email: 'xxx@xxx.com',
+  twitter: 'https://twitter.com/sudhanshutwt',
+  facebook: 'xxx',
+  linkedin: 'https://www.linkedin.com/in/sudhanshu-tripathi77/'
+}
