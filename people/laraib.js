43a993811528c0bdeeec8ae5a2216fdcaccd001e
LARAIB <114015411+laraibnoorien@users.noreply.github.com>
2023-10-23 11:58:02 +0530

    Create laraib.js

---
+++ people/laraib.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'Laraib Noorien',
+    github: 'https://github.com/laraibnoorien',
+    email: 'laraibnoorien@gmail.com',
+    twitter: 'no link',
+    facebook: 'no link',
+    linkedin: 'no link'
+  }
+  
