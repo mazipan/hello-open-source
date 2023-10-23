10f8abadfde02b3cfbc4b2c581e47e3ea4599af6
Shreya Shambhavi <118973922+Shambhavi20031984@users.noreply.github.com>
2023-10-23 12:35:56 +0530

    Update Shreya.js

--- people/Shreya.js
+++ people/Shreya.js
@@ -1,5 +1,5 @@
 module.exports = {
-    name: 'Shreya',
+    name: 'Shreya Shambhavi',
     github: 'https://github.com/Shambhavi20031984',
     linkedin: 'www.linkedin.com/in/shreya-shambhavi-324985h3268'
-  }
+ }
