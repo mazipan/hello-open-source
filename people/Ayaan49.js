e3a87db292aa009cb177fec292955d831a7c1781
Ayaan49 <ayaanbordoloi25@gmail.com>
2023-10-24 16:45:46 +0530

    Add my data

---
+++ people/Ayaan49.js
@@ -0,0 +1,7 @@
+module.exports = {
+    name: 'Ayaan',
+    github: 'https://github.com/Ayaan49',
+    email: 'ayaansive25@gmail.com',
+    twitter: 'https://twitter.com/twtayaan',
+    linkedin: 'https://www.linkedin.com/in/ayaan49/'
+  }
