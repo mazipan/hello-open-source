9f8f4afbf615d0b770e477ebb8af1954c0512d96
Aastha Agarwal <114092619+aasthaagarwal2003@users.noreply.github.com>
2023-10-25 01:48:55 +0530

    Create aastha.js

---
+++ people/aastha.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Aastha Agarwal',
+  github: 'aasthaagarwal2003',
+  email: 'aasthaagarwal5203@gmail.com',
+  twitter: '@aastha2003',
+  linkedin: 'https://www.linkedin.com/in/aastha-agarwal-940b29256?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base_contact_details%3BbRvki6lvTi%2BW7%2BQlvHfS2g%3D%3D'
+}
