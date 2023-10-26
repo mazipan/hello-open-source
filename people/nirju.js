7d4b47db496bb8843867cb1080efd0f3ab5bfb5a
nirju123 <147715768+nirju123@users.noreply.github.com>
2023-10-26 20:17:43 +0530

    Added nirju.js

---
+++ people/nirju.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Nirju Pirju',
+  github: 'https://github.com/nirju123',
+  email: 'nirju526@gmail.com',
+  twitter: '@uiol_nirju',
+  facebook: '@uiol_nirju',
+  linkedin: 'in/uiol_nirju-a006ab202/'
+}
