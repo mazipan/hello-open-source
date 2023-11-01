95fab4893122afe3b6c98ab3042a760f6649acc0
mazipan <mazipanneh@gmail.com>
2023-11-01 14:00:53 +0000

    🔥 CHORE: purge all data

--- people/SuraniHarsh.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'Harsh Surani',
-    github: 'https://github.com/SuraniHarsh',
-    email : 'harshsurani00@gmail.com',
-    twitter: '@xxx',
-    facebook: 'xxx',
-    linkedin: 'in/xxx'
-}
