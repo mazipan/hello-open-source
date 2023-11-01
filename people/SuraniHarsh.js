3f6c5778db597415703094dca4c8ce39d3245d31
SuraniHarsh <harshsurani00@gmail.com>
2023-11-01 18:13:18 +0530

    Add HarshSurani.js under people directory

---
+++ people/SuraniHarsh.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Harsh Surani',
+    github: 'https://github.com/SuraniHarsh',
+    email : 'harshsurani00@gmail.com',
+    twitter: '@xxx',
+    facebook: 'xxx',
+    linkedin: 'in/xxx'
+}
