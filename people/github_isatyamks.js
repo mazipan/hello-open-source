028b5afd8c6c32503e6b44ac1a0b626d2e691fe6
Satyam Kumar Singh <isatyamks@gmail.com>
2023-11-14 20:00:32 +0530

    Create github_isatyamks.js

---
+++ people/github_isatyamks.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Satyam Kumar Singh',
+  github: 'isatyamks',
+  email: 'isatyamks+git@gmail.com',
+  twitter: '@isatyamks',
+  facebook: '@isatyamks',
+  linkedin: 'in/isatyamks'
+}
