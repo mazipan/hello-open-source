762f4e2b8ae0c761db38bb643a6fbeb32287f7c1
Prasanna <prasannasparkle1225@gmail.com>
2023-11-24 10:59:16 +0530

    updated my details

---
+++ people/prasanna1225.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'Prasanna Donga',
+    github: 'prasanna1225',
+    email: 'prasannadonga1357@gmail.com',
+    twitter: '@prasannaa',
+    facebook: 'xxx',
+    linkedin: 'prasanna donga'
+  }
