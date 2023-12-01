f2714370f24804af054789a0e980529a07600490
mazipan <mazipanneh@gmail.com>
2023-12-01 14:00:30 +0000

    🔥 CHORE: purge all data

--- people/github_isatyamks.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Satyam Kumar Singh',
-  github: 'isatyamks',
-  email: 'isatyamks+git@gmail.com',
-  twitter: '@isatyamks',
-  facebook: '@isatyamks',
-  linkedin: 'in/isatyamks'
-}
--- people/prasanna1225.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'Prasanna Donga',
-    github: 'prasanna1225',
-    email: 'prasannadonga1357@gmail.com',
-    twitter: '@prasannaa',
-    facebook: 'xxx',
-    linkedin: 'prasanna donga'
-  }
