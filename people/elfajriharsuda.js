a72cd142f1876add20be04e5e7eab2bb1bcb7cab
Elfajri Harsuda <elfajri.harsuda@gmail.com>
2023-12-24 20:26:43 +0700

    added elfajriharsuda.js

---
+++ people/elfajriharsuda.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'ELFAJRI_HARSUDA',
+    github: 'elfajriharsuda',
+    email: 'elfajri.harsuda@gmail.com',
+    twitter: '-',
+    facebook: 'elfajriharsuda',
+    linkedin: 'in/elfajriharsuda/'
+  }
