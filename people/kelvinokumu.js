d51ec4e1546d669af9c96ee5e063737fdf23b8fc
kelvinokumu <oumakelvinokumu@gmail.com>
2024-01-06 13:38:56 +0300

    added my details

---
+++ people/kelvinokumu.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'kelvinokumu',
+    github: 'https://github.com/kelvinokumu',
+    email: 'oumakelvinokumu@gmail.com',
+    twitter: '@OkushKelvin',
+    facebook: 'xxxxxxxx',
+    linkedin: 'https://www.linkedin.com/in/kelvin-okumu-965bb0147/'
+  }
