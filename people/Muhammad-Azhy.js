265b609b93de3c76b479b55cd78d6c7c71fd70a2
Muhammad-Azhy <114934452+Muhammad-Azhy@users.noreply.github.com>
2024-03-30 13:31:17 +0300

    Added Muhammad Azhy

---
+++ people/Muhammad-Azhy.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'Muhammad Azhy',
+  github: 'https://github.com/Muhammad-Azhy',
+  email: 'hamaaudi651@gmail.com',
+  twitter: '@xxx',
+  facebook: 'xxx',
+  linkedin: 'in/muhammed-azhy'
+}
