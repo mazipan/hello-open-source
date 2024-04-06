e2623e0c7aa31d9c18430ee26a82732b9466f150
Muhammad-Azhy <114934452+Muhammad-Azhy@users.noreply.github.com>
2024-04-06 10:50:50 +0300

    __

--- people/Muhammad-Azhy.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Muhammad Azhy',
-  github: 'https://github.com/Muhammad-Azhy',
-  email: 'hamaaudi651@gmail.com',
-  twitter: '@xxx',
-  facebook: 'xxx',
-  linkedin: 'in/muhammed-azhy'
-}
