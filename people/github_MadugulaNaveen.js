86542a7fa358c515bd88b6008e41ddc82a70d75a
Naveen Madugula <naveenmadugula7630@gmail.com>
2024-08-27 22:52:00 +0530

    Add github_MadugulaNaveen.js to People Directory

---
+++ people/github_MadugulaNaveen.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'MadugulaNaveen',
+  github: 'MadugulaNaveen',
+  email: 'naveen@gmail.com',
+  twitter: '@naveenMadugula',
+  facebook: '..',
+  linkedin: 'in/NaveenMadugula'
+}
