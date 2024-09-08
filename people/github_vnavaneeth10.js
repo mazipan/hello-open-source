70d62999bbf9cfeb53cd06a5ad509f3ab7a2667c
Navaneeth V <navaneethnair69@gmail.com>
2024-09-08 23:01:50 +0530

    added a new file

---
+++ people/github_vnavaneeth10.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'navaneeth',
+    github: 'vnavaneeth10',
+    email: 'navaneeth@gmail.com',
+    twitter: '@navaneeth',
+    facebook: 'navaneeth',
+    linkedin: 'in/navaneeth'
+  }
