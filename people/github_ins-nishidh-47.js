8fd0035ef68a1811f0a33fa3229a49b85a372737
ins-nishidh-47 <nishidhsingh97@gmail.com>
2024-12-19 12:58:42 +0530

    Added Malayalam readme file

---
+++ people/github_ins-nishidh-47.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'Nishidh Singh',
+    github: 'https://github.com/ins-nishidh-47',
+    email: 'nishidhsingh97@gmail.com',
+    twitter: '@ins_Nishidh',
+    facebook: 'https://www.facebook.com',
+    linkedin: 'https://www.linkedin.com/in/nishidh-s-060571291/',
+  }
+  
--- people/github_piyushbhatt.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    name: 'Piyush Bhatt',
-    github: 'https://github.com/Piyushbhatt7',
-    email: 'piyushbhatt162@gmail.com',
-    twitter: '@Kunalsharma',
-    facebook: 'https://www.facebook.com/profile.php?id=10088761335930504',
-    linkedin: 'https://www.linkedin.com/in/piyush-bhatt-024225253/',
-  }
-  
