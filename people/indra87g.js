3f39f0e935fc2b8452ec00e51ba3586b364c5a28
Indra Sah Noeldy <twinightwheel50@gmail.com>
2025-03-02 12:54:57 +0700

    Create indra87g.js

---
+++ people/indra87g.js
@@ -0,0 +1,8 @@
+module.exports = {
+  name: 'TwinightWheel50',
+  github: 'indra87g',
+  email: 'echo@indra87g.space',
+  twitter: '@indra87g',
+  facebook: 'indra87g',
+  linkedin: 'in/pengangguran',
+}
