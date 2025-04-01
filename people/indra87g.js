a7aa90e87f7816767fbaad8247481efb75824d95
mazipan <mazipanneh@gmail.com>
2025-04-01 14:07:59 +0000

    🔥 CHORE: purge all data

--- people/indra87g.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'TwinightWheel50',
-  github: 'indra87g',
-  email: 'echo@indra87g.space',
-  twitter: '@indra87g',
-  facebook: 'indra87g',
-  linkedin: 'in/pengangguran',
-}
--- people/turki013.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'turki',
-    github: 'turki013',
-    email: 'trkee112299@gmail.com',
-    twitter: '@Fx_rg13',
-    facebook: 'nothing',
-    linkedin: 'Turki AL-Mutairi'
-  }
