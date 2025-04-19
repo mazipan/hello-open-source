b1460e6886d9deb015bf9c2fae2d5c0b48b9919b
just-rakesh <dasrakhi2d@gmail.com>
2025-04-20 01:27:31 +0530

    add my file

---
+++ people/rcd_env.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'RCD',
+    github: 'rcd-env',
+    email: 'hisashiburidana@mugiwara.luffy',
+    twitter: '@x.com',
+    facebook: 'nani',
+    linkedin: 'oops 404',
+  }
+  
