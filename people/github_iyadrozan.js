8814e91736a6f54ec3d2e627fcc4cb34930b8f6f
iyadrozan <iyadrozan26@gmail.com>
2025-04-23 23:34:14 +0800

    add iyadrozan

---
+++ people/github_iyadrozan.js
@@ -0,0 +1,8 @@
+module.exports = {
+    name: 'iyad rozan',
+    github: 'github.com/iyadrozan',
+    email: 'iyadrozan26@gmail.com',
+    twitter: '@iyadrozan',
+    linkedin: 'in/iyadrozan',
+  }
+  
