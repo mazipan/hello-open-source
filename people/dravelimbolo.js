20c0eefc9e66e1767f7259dd668d54e0dc2607a4
Vrinda Joshi <vrinda.joshi@hotmail.com>
2025-11-09 16:14:12 -0500

    fix: remove user data

--- people/dravelimbolo.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  name: 'Dravel IMBOLO',
-  github: 'dravelimbolo',
-  email: 'contact@dravelimbolo.com',
-  twitter: '@dravelimbolo',
-  facebook: 'drav.imbolo',
-  linkedin: 'in/dravel-imbolo'
-};
