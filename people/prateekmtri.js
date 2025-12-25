ddf4ec95c2d4e25025b68d8a303f029971f06c0a
prateekmtri <prateek1tri2@gmail.com>
2025-12-25 20:32:43 +0530

    Add prateekmtri profile

---
+++ people/prateekmtri.js
@@ -0,0 +1,7 @@
+module.exports = {
+  name: 'Prateek Mani Tripathi',
+  github: 'prateekmtri',
+  email: 'prateek1tri2@gmail.com',
+  twitter: '@prateek_tripathi__23',
+  linkedin: 'https://www.linkedin.com/in/prateek-mani-tripathi-51935a259/'
+}
